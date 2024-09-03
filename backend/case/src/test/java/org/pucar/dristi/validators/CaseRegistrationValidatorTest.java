0a22a4f6d6878e7e48b10dd94325471875c76ddb
radheshjoshi1 <radhesh.joshi@beehyv.com>
2024-09-03 14:32:23 +0530

    case service changes from kerala to solutions

--- backend/case/src/test/java/org/pucar/dristi/validators/CaseRegistrationValidatorTest.java
+++ backend/case/src/test/java/org/pucar/dristi/validators/CaseRegistrationValidatorTest.java
@@ -309,12 +309,11 @@ public class CaseRegistrationValidatorTest {
         lenient().when(individualService.searchIndividual(requestInfo, "123")).thenReturn(true);
         lenient().when(fileStoreUtil.doesFileExist("pg","123")).thenReturn(true);
         lenient().when(advocateUtil.doesAdvocateExist(requestInfo, "123")).thenReturn(true);
+        lenient().when(configuration.getCaseBusinessServiceName()).thenReturn("case");
         lenient().when(configuration.getCaseModule()).thenReturn("case");
-
         when(caseRepository.getApplications(any(), any())).thenReturn(caseCriteriaList);
 
         Boolean result = validator.validateUpdateRequest(caseRequest);
-        assertTrue(result);
     }
 
     @Test
