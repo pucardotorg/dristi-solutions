1ea193678eb02400db15ff3a6bd2d09801be0972
tushar.k <tushar.k@beehyv.com>
2024-09-09 20:03:22 +0530

    uat optout fix, first name (min 1) and last name optional #1514

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/ScheduleNextHearing.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/ScheduleNextHearing.js
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Button, CardText, CustomDropdown, SubmitBar, TextInput, Toast, Modal, Loader, Banner } from "@egovernments/digit-ui-react-components";
 import { formatDateInMonth } from "../../utils";
 import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import useSearchCaseService from "@egovernments/digit-ui-module-dristi/src/hooks/dristi/useSearchCaseService";
 import { HomeService, Urls } from "../../hooks/services";
 import { InfoCard } from "@egovernments/digit-ui-components";
@@ -183,6 +183,8 @@ function ScheduleNextHearing({
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(false);
   const [sucessOptOut, setSucessOptOut] = useState(false);
   const [OptOutLimitValue, setOptOutLimitValue] = useState(null);
+  const location = useLocation();
+  const referenceId = location?.state?.state?.params?.referenceId;
 
   const CustomCaseInfoDiv = Digit.ComponentRegistryService.getComponent("CustomCaseInfoDiv") || <React.Fragment></React.Fragment>;
   const CustomChooseDate = Digit.ComponentRegistryService.getComponent("CustomChooseDate") || <React.Fragment></React.Fragment>;
@@ -238,14 +240,17 @@ function ScheduleNextHearing({
   );
 
   const { data: dateResponse } = Digit.Hooks.home.useSearchReschedule(
-    applicationData
-      ? {
-          SearchCriteria: {
-            tenantId: Digit.ULBService.getCurrentTenantId(),
-            rescheduledRequestId: [applicationData.applicationList[0]?.applicationNumber],
-          },
-        }
-      : null,
+    {
+      SearchCriteria: {
+        tenantId: Digit.ULBService.getCurrentTenantId(),
+        ...(applicationData?.applicationList?.length > 0
+          ? { rescheduledRequestId: [applicationData.applicationList[0]?.applicationNumber] }
+          : {
+              hearingBookingId: referenceId,
+              status: "ACTIVE",
+            }),
+      },
+    },
     { limit: 5, offset: 0 },
     "",
     !!applicationData
@@ -285,7 +290,7 @@ function ScheduleNextHearing({
   };
 
   const handleClose = () => {
-    history.push(`/${window?.contextPath}/${userInfoType}/home/home-pending-task`, { taskType: { code: "case", name: "Case" } });
+    history.goBack();
   };
 
   const handleSubmit = async (data) => {
@@ -395,7 +400,7 @@ function ScheduleNextHearing({
 
   return (
     <Modal
-      headerBarMain={<Heading label={status === "OPTOUT" ? "Select Opt-out Dates" : t(config.headModal)} />}
+      headerBarMain={<Heading label={status === "OPTOUT" ? t("SELECT_OPT_OUT_DATES") : t(config.headModal)} />}
       headerBarEnd={<CloseBtn onClick={handleClose} />}
       hideSubmit={true}
       popupStyles={{
