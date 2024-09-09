d90c9cbddd377fb373c6014e83b046cbb4ba6805
Ajith-beehyv <150236657+Ajith-beehyv@users.noreply.github.com>
2024-09-09 16:03:19 +0530

    case sms and pdf changes migration (#84)
    * case sms and pdf changes migration
    
    * resolved some issues raised by code rabbit

--- backend/case/src/main/java/org/pucar/dristi/service/IndividualService.java
+++ backend/case/src/main/java/org/pucar/dristi/service/IndividualService.java
@@ -3,6 +3,7 @@ package org.pucar.dristi.service;
 import static org.pucar.dristi.config.ServiceConstants.INDIVIDUAL_SERVICE_EXCEPTION;
 
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.common.models.individual.Individual;
 import org.egov.tracer.model.CustomException;
 import org.pucar.dristi.config.Configuration;
 import org.pucar.dristi.util.IndividualUtil;
@@ -12,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -45,6 +49,31 @@ public class IndividualService {
         } catch (Exception e) {
             log.error("Error in search individual service :: {}",e.toString());
             throw new CustomException(INDIVIDUAL_SERVICE_EXCEPTION, "Error in search individual service" + e.getMessage());
+        }
+    }
+
+    public List<Individual> getIndividuals(RequestInfo requestInfo, List<String> uuids) {
+        try {
+            IndividualSearchRequest individualSearchRequest = new IndividualSearchRequest();
+            individualSearchRequest.setRequestInfo(requestInfo);
+            IndividualSearch individualSearch = new IndividualSearch();
+            individualSearch.setUserUuid(uuids);
+            individualSearchRequest.setIndividual(individualSearch);
+            StringBuilder uri = new StringBuilder(config.getIndividualHost()).append(config.getIndividualSearchEndpoint());
+            uri.append("?limit=").append(uuids.size()).append("&offset=0")
+                    .append("&tenantId=").append(requestInfo.getUserInfo().getTenantId())
+                    .append("&includeDeleted=true");
+            List<Individual> individual = individualUtils.getIndividualByIndividualId(individualSearchRequest, uri);
+            if (individual != null ) {
+                return individual;
+            } else {
+                log.error("No individuals found");
+                return Collections.emptyList();
+            }
+        } catch (Exception e) {
+            log.error("Error in search individual service: ", e);
+            log.error("Individuals not found");
+            return Collections.emptyList();
         }
     }
 }
