4f4b5930e4793172cc77f47b79df1d0fbe01d117
tushar.k <tushar.k@beehyv.com>
2024-09-11 15:50:23 +0530

    dependabot security issue fixes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/Utils/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/Utils/index.js
@@ -33,7 +33,7 @@ export const ServiceRequest = async ({
 
 export function generateUUID() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0;
+    const r = (window.crypto.getRandomValues(new Uint32Array(1))[0] * Math.pow(2, -32) * 16) | 0;
     const v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
