18f0cb82825be09ef15c8e9f8d7c1200dddc72ee
vaibhavc <vaibhav.c@beehyv.com>
2024-09-12 15:46:44 +0530

    replacing caseNumber with cnrNumber - code rabit suggestion

--- utilities/dristi-pdf/src/applicationHandlers/applicationCheckout.js
+++ utilities/dristi-pdf/src/applicationHandlers/applicationCheckout.js
@@ -203,7 +203,7 @@ async function applicationCheckout(req, res, qrCode) {
         {
           courtComplex: mdmsCourtRoom.name,
           caseType: "Negotiable Instruments Act 138 A",
-          caseNumber: courtCase.cnrNumber,
+          caseNumber: courtCase.caseNumber,
           caseYear: caseYear,
           caseName: courtCase.caseTitle,
           judgeName: "John Doe", // FIXME: employee.user.name
