2117cd83aa5b3ac49c9a4dc920c3bd2e6ea86e3a
hemadri <hemadri.pragada@beehyv.com>
2024-09-12 12:52:29 +0530

    added not null

--- backend/scheduler-svc/src/main/resources/db/migration/main/V20240912115200__oo_opt_out_id_ddl.sql
+++ backend/scheduler-svc/src/main/resources/db/migration/main/V20240912115200__oo_opt_out_id_ddl.sql
@@ -1,5 +1,5 @@
 ALTER TABLE reschedule_request_opt_out_detail
-ADD COLUMN id varchar(64);
+ADD COLUMN id varchar(64) NOT NULL;
 
 ALTER TABLE reschedule_request_opt_out_detail
 ADD CONSTRAINT unique_id UNIQUE (id);
