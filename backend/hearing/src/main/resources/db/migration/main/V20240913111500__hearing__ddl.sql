20335ceddd733fea2bbc434c5bc053241aae5d4e
radheshjoshi1 <radhesh.joshi@beehyv.com>
2024-09-13 11:56:54 +0530

    removed indexes for some fields

--- backend/hearing/src/main/resources/db/migration/main/V20240913111500__hearing__ddl.sql
+++ backend/hearing/src/main/resources/db/migration/main/V20240913111500__hearing__ddl.sql
@@ -7,6 +7,3 @@ CREATE INDEX IF NOT EXISTS idx_dristi_hearing_attendees ON dristi_hearing USING 
 CREATE INDEX IF NOT EXISTS idx_drist_hearing_start_time ON dristi_hearing (startTime);
 
 CREATE INDEX IF NOT EXISTS idx_dristi_hearing_document_hearing_id ON dristi_hearing_document (hearingId);
-CREATE INDEX IF NOT EXISTS idx_dristi_hearing_document_document_type ON dristi_hearing_document (documentType);
-CREATE INDEX IF NOT EXISTS idx_dristi_hearing_document_file_store ON dristi_hearing_document (fileStore);
-CREATE INDEX IF NOT EXISTS idx_dristi_hearing_document_document_uid ON dristi_hearing_document (documentUid);
