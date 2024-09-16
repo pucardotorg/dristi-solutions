8fc7ae2fe5f83ef21ae43aed79ffbee969b66ae1
radheshjoshi1 <radhesh.joshi@beehyv.com>
2024-09-16 11:05:58 +0530

    gin index change

--- backend/hearing/src/main/resources/db/migration/main/V20240913111500__hearing__ddl.sql
+++ backend/hearing/src/main/resources/db/migration/main/V20240913111500__hearing__ddl.sql
@@ -3,7 +3,7 @@ CREATE INDEX IF NOT EXISTS idx_dristi_hearing_filing_number ON dristi_hearing US
 CREATE INDEX IF NOT EXISTS idx_dristi_hearing_cnr_number ON dristi_hearing USING GIN (cnrNumbers);
 CREATE INDEX IF NOT EXISTS idx_dristi_hearing_application_number ON dristi_hearing USING GIN (applicationNumbers);
 CREATE INDEX IF NOT EXISTS idx_dristi_hearing_hearing_type ON dristi_hearing (hearingType);
-CREATE INDEX IF NOT EXISTS idx_dristi_hearing_attendees ON dristi_hearing USING GIN (attendees);
+CREATE INDEX IF NOT EXISTS idx_dristi_hearing_individual_id ON dristi_hearing USING gin (attendees jsonb_path_ops);
 CREATE INDEX IF NOT EXISTS idx_drist_hearing_start_time ON dristi_hearing (startTime);
 
 CREATE INDEX IF NOT EXISTS idx_dristi_hearing_document_hearing_id ON dristi_hearing_document (hearingId);
