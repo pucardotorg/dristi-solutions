330dda229d59203d3879cbc823c2281215f5fd2b
utpal.kant <utpal.kant@beehyv.com>
2024-09-27 14:09:11 +0530

    case Number changes

--- utilities/dristi-pdf/src/orderHandlers/orderNotice.js
+++ utilities/dristi-pdf/src/orderHandlers/orderNotice.js
@@ -141,6 +141,8 @@ async function orderNotice(req, res, qrCode) {
       ? formatDate(new Date(order?.orderDetails?.hearingDate), "DD-MM-YYYY")
       : "";
     const partyName = order?.orderDetails?.respondentName || "";
+    const caseNumber = courtCase?.courtCaseNumber || courtCase?.cmpNumber || "";
+
     const data = {
       Data: [
         {
@@ -148,7 +150,7 @@ async function orderNotice(req, res, qrCode) {
           place: mdmsCourtRoom.place,
           state: mdmsCourtRoom.state,
           caseName: courtCase.caseTitle,
-          caseNumber: courtCase.caseNumber,
+          caseNumber: caseNumber,
           partyName: partyName,
           typeOfNotice: typeOfNotice,
           hearingDate: hearingDate,
