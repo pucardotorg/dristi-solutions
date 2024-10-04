59e261014fa67d06e330eeb2ffd13043431515dd
maruthi phanendra <phanendra.chaluvadi@beehyv.com>
2024-10-04 11:33:37 +0530

    updated the size of top header also

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/LanguageSelection.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/LanguageSelection.js
@@ -52,8 +52,8 @@ const LanguageSelection = () => {
   ) : (
     <div className="selection-card-wrapper langauge-select-wrapper">
       <PageBasedInput style={getLanguageSelectionStyles()} texts={texts} onSubmit={onSubmit}>
-        <CardHeader styles={{ margin: 0 , textAlign: "center"}}>{t("CS_COMMON_CHOOSE_LANGUAGE")}</CardHeader>
-        <CardHeader styles={{ margin: 0,   fontSize: "18px"}} >{t("CS_COMMON_CHOOSE_LANGUAGE_ML")}</CardHeader>
+        <CardHeader styles={{ margin: 0 , textAlign: "center" ,fontSize: "48px"}}>{t("CS_COMMON_CHOOSE_LANGUAGE")}</CardHeader>
+        <CardHeader styles={{ margin: 0 , textAlign: "center" ,fontSize: "48px"}} >{t("CS_COMMON_CHOOSE_LANGUAGE_ML")}</CardHeader>
         <RadioButtons {...RadioButtonProps} />
       </PageBasedInput>
     </div>
