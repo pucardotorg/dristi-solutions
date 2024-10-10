fad78c0774bb37718c046d1a59de02ec19205ecb
vaibhavc <vaibhavc@beehyv.com>
2024-10-10 18:14:03 +0530

    png, jpg, jepg files added

--- backend/dristi-case-pdf/routes/pdfRoutes.js
+++ backend/dristi-case-pdf/routes/pdfRoutes.js
@@ -4,42 +4,50 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const { PDFDocument } = require("pdf-lib");
 const fs = require("fs");
+const path = require("path");
 
 router.post(
   "/combine-documents",
   upload.array("documents"),
   async (req, res) => {
     try {
-      const files = req.files;
       const mergedPdf = await PDFDocument.create();
-      for (const file of files) {
-        const pdfBytes = fs.readFileSync(file.path);
-        const pdfDoc = await PDFDocument.load(pdfBytes);
-        const pages = await mergedPdf.copyPages(
-          pdfDoc,
-          pdfDoc.getPageIndices()
-        );
-        pages.forEach((page) => mergedPdf.addPage(page));
-      }
 
+      for (const file of req.files) {
+        const ext = path.extname(file.originalname).toLowerCase();
+        const fileData = fs.readFileSync(file.path);
+
+        if (ext === ".pdf") {
+          const pdfDoc = await PDFDocument.load(fileData);
+          const pages = await mergedPdf.copyPages(
+            pdfDoc,
+            pdfDoc.getPageIndices()
+          );
+          pages.forEach((page) => mergedPdf.addPage(page));
+        } else if ([".jpg", ".jpeg", ".png"].includes(ext)) {
+          const img =
+            ext === ".png"
+              ? await mergedPdf.embedPng(fileData)
+              : await mergedPdf.embedJpg(fileData);
+          const imgDims = img.scale(1);
+          const page = mergedPdf.addPage([imgDims.width, imgDims.height]);
+          page.drawImage(img, {
+            x: 0,
+            y: 0,
+            width: imgDims.width,
+            height: imgDims.height,
+          });
+        }
+
+        fs.unlinkSync(file.path); // Clean up files after processing
+      }
       const mergedPdfBytes = await mergedPdf.save();
       const finalPdfBuffer = Buffer.from(mergedPdfBytes);
-      files.forEach((file) => fs.unlinkSync(file.path));
-
       res.set({
         "Content-Type": "application/pdf",
         "Content-Disposition": "attachment; filename=merged.pdf",
       });
       res.send(finalPdfBuffer);
-      files.forEach((file) => {
-        fs.unlink(file.path, (err) => {
-          if (err) {
-            console.error(`Error deleting file ${file.path}:`, err);
-          } else {
-            console.log(`Deleted file: ${file.path}`);
-          }
-        });
-      });
     } catch (error) {
       console.error("Error during PDF merging:", error); // Log the error for debugging
 
