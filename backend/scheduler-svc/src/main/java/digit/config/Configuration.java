7ffc8f0e074d0095f57297f9b6bec4f0ec1982d7
shashikesh12 <shashikesh.r@beehyv.com>
2024-10-17 17:37:25 +0530

    Changes for issue Audit logs not reflecting in DB

--- backend/scheduler-svc/src/main/java/digit/config/Configuration.java
+++ backend/scheduler-svc/src/main/java/digit/config/Configuration.java
@@ -212,4 +212,7 @@ public class Configuration {
 
     @Value("${judge.designation}")
     private String judgeDesignation;
+
+    @Value("${state.level.tenant.id}")
+    private String stateLevelTenantId;
 }
