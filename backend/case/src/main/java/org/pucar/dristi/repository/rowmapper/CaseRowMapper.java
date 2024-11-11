ea94b227abcce87a2fd6a0795f03a99b8fda9e09
prasanna2400 <prasannajeet.v@beehyv.com>
2024-11-11 16:45:39 +0530

    updated row mapper to avoid 0 as date

--- backend/case/src/main/java/org/pucar/dristi/repository/rowmapper/CaseRowMapper.java
+++ backend/case/src/main/java/org/pucar/dristi/repository/rowmapper/CaseRowMapper.java
@@ -1,20 +1,9 @@
 package org.pucar.dristi.repository.rowmapper;
 
-import static org.pucar.dristi.config.ServiceConstants.ROW_MAPPER_EXCEPTION;
-
-import java.io.IOException;
-import java.sql.ResultSet;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.models.AuditDetails;
 import org.egov.tracer.model.CustomException;
 import org.postgresql.util.PGobject;
@@ -22,9 +11,14 @@ import org.pucar.dristi.web.models.CourtCase;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
 
-import lombok.extern.slf4j.Slf4j;
+import static org.pucar.dristi.config.ServiceConstants.ROW_MAPPER_EXCEPTION;
 
 @Component
 @Slf4j
@@ -73,9 +67,9 @@ public class CaseRowMapper implements ResultSetExtractor<List<CourtCase>> {
                             .stage(rs.getString("stage"))
                             .isActive(rs.getBoolean("isactive"))
                             .substage(rs.getString("substage"))
-                            .filingDate(rs.getLong("filingdate"))
-                            .judgementDate(rs.getLong("judgementdate"))
-                            .registrationDate(rs.getLong("registrationdate"))
+                            .filingDate(rs.getString("filingdate") == null ? null : Long.valueOf(rs.getString("filingdate")))
+                            .judgementDate(rs.getString("judgementdate") == null ? null : Long.valueOf(rs.getString("judgementdate")))
+                            .registrationDate(rs.getString("registrationdate") == null ? null : Long.valueOf(rs.getString("registrationdate")))
                             .caseCategory(rs.getString("casecategory"))
                             .natureOfPleading(rs.getString("natureofpleading"))
                             .status(rs.getString("status"))
@@ -101,31 +95,33 @@ public class CaseRowMapper implements ResultSetExtractor<List<CourtCase>> {
         }
         return new ArrayList<>(caseMap.values());
     }
-        public <T> T getObjectFromJson(String json, TypeReference<T> typeRef) {
-            log.info("Converting JSON to type: {}", typeRef.getType());
-            log.info("JSON content: {}", json);
 
-            try {
-                if (json == null || json.trim().isEmpty()) {
-                    if (isListType(typeRef)) {
-                        return (T) new ArrayList<>(); // Return an empty list for list types
-                    } else {
-                        return objectMapper.readValue("{}", typeRef); // Return an empty object for other types
-                    }
+    public <T> T getObjectFromJson(String json, TypeReference<T> typeRef) {
+        log.info("Converting JSON to type: {}", typeRef.getType());
+        log.info("JSON content: {}", json);
+
+        try {
+            if (json == null || json.trim().isEmpty()) {
+                if (isListType(typeRef)) {
+                    return (T) new ArrayList<>(); // Return an empty list for list types
+                } else {
+                    return objectMapper.readValue("{}", typeRef); // Return an empty object for other types
                 }
-
-                // Attempt to parse the JSON
-                return objectMapper.readValue(json, typeRef);
-            } catch (IOException e) {
-                log.error("Failed to convert JSON to {}", typeRef.getType(), e);
-                throw new CustomException("Failed to convert JSON to " + typeRef.getType(), e.getMessage());
             }
-        }
 
-        private <T> boolean isListType(TypeReference<T> typeRef) {
-            Class<?> rawClass = TypeFactory.defaultInstance().constructType(typeRef.getType()).getRawClass();
-            return List.class.isAssignableFrom(rawClass);
+            // Attempt to parse the JSON
+            return objectMapper.readValue(json, typeRef);
+        } catch (IOException e) {
+            log.error("Failed to convert JSON to {}", typeRef.getType(), e);
+            throw new CustomException("Failed to convert JSON to " + typeRef.getType(), e.getMessage());
         }
+    }
+
+    private <T> boolean isListType(TypeReference<T> typeRef) {
+        Class<?> rawClass = TypeFactory.defaultInstance().constructType(typeRef.getType()).getRawClass();
+        return List.class.isAssignableFrom(rawClass);
+    }
+
     private LocalDate stringToLocalDate(String str) {
         LocalDate localDate = null;
         try {
