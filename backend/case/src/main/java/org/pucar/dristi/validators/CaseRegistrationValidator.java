b71cedf8a005a5f68704499e79563c9d1bcca75f
harshkumar8789 <harsh.kumar@beehyv.com>
2024-11-13 18:47:05 +0530

    remove unwanted import.

--- backend/case/src/main/java/org/pucar/dristi/validators/CaseRegistrationValidator.java
+++ backend/case/src/main/java/org/pucar/dristi/validators/CaseRegistrationValidator.java
@@ -4,7 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
+import static org.pucar.dristi.config.ServiceConstants.DELETE_DRAFT_WORKFLOW_ACTION;
+import static org.pucar.dristi.config.ServiceConstants.INDIVIDUAL_NOT_FOUND;
+import static org.pucar.dristi.config.ServiceConstants.INVALID_ADVOCATE_DETAILS;
+import static org.pucar.dristi.config.ServiceConstants.INVALID_ADVOCATE_ID;
+import static org.pucar.dristi.config.ServiceConstants.INVALID_COMPLAINANT_DETAILS;
+import static org.pucar.dristi.config.ServiceConstants.INVALID_DOCUMENT_DETAILS;
+import static org.pucar.dristi.config.ServiceConstants.INVALID_FILESTORE_ID;
+import static org.pucar.dristi.config.ServiceConstants.INVALID_LINKEDCASE_ID;
+import static org.pucar.dristi.config.ServiceConstants.MDMS_DATA_NOT_FOUND;
+import static org.pucar.dristi.config.ServiceConstants.SAVE_DRAFT_CASE_WORKFLOW_ACTION;
+import static org.pucar.dristi.config.ServiceConstants.SUBMIT_CASE_WORKFLOW_ACTION;
+import static org.pucar.dristi.config.ServiceConstants.VALIDATION_ERR;
+import static org.pucar.dristi.config.ServiceConstants.SUBMIT_CASE_ADVOCATE_WORKFLOW_ACTION;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.tracer.model.CustomException;
 import org.pucar.dristi.config.Configuration;
@@ -25,7 +37,6 @@ import org.springframework.util.ObjectUtils;
 
 import net.minidev.json.JSONArray;
 
-import static org.pucar.dristi.config.ServiceConstants.*;
 
 @Component
 public class CaseRegistrationValidator {
