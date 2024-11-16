c98a3dd87207c9568b8a4d37a96288a952b562b1
harshkumar8789 <harsh.kumar@beehyv.com>
2024-11-16 17:14:57 +0530

    sms-template implementation in order service.

--- backend/order/src/main/java/org/pucar/dristi/util/CaseUtil.java
+++ backend/order/src/main/java/org/pucar/dristi/util/CaseUtil.java
@@ -1,12 +1,15 @@
 package org.pucar.dristi.util;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.tracer.model.CustomException;
 import org.pucar.dristi.config.Configuration;
 import org.pucar.dristi.web.models.CaseExists;
 import org.pucar.dristi.web.models.CaseExistsRequest;
 import org.pucar.dristi.web.models.CaseExistsResponse;
+import org.pucar.dristi.web.models.CaseSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +18,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static org.pucar.dristi.config.ServiceConstants.ERROR_WHILE_FETCHING_FROM_CASE;
 
 @Slf4j
 @Component
@@ -59,4 +64,21 @@ public class CaseUtil {
 			return false;
 		return caseResponse.getCriteria().get(0).getExists();
 	}
+
+	public JsonNode searchCaseDetails(CaseSearchRequest caseSearchRequest) {
+		StringBuilder uri = new StringBuilder();
+		uri.append(configs.getCaseHost()).append(configs.getCaseSearchPath());
+
+		Object response = new HashMap<>();
+		try {
+			response = restTemplate.postForObject(uri.toString(), caseSearchRequest, Map.class);
+			JsonNode jsonNode = mapper.readTree(mapper.writeValueAsString(response));
+			JsonNode caseList = jsonNode.get("criteria").get(0).get("responseList");
+			return caseList.get(0);
+		} catch (Exception e) {
+			log.error(ERROR_WHILE_FETCHING_FROM_CASE, e);
+			throw new CustomException(ERROR_WHILE_FETCHING_FROM_CASE, e.getMessage());
+		}
+	}
+
 }
