0e3ce30e0a11723f016753a48894becc34ae9930
hemadri <hemadri.pragada@beehyv.com>
2024-11-19 12:05:48 +0530

    Requestinfo spelling update, and updated workflow transaction value

--- backend/casemanagement/src/main/java/org/pucar/dristi/service/CaseBundleIndexBuilderService.java
+++ backend/casemanagement/src/main/java/org/pucar/dristi/service/CaseBundleIndexBuilderService.java
@@ -123,7 +123,7 @@ public class CaseBundleIndexBuilderService {
         String businessId =null;
         String tenantId=null ;
         String stateName=null;
-        RequestInfo requestInfo = (RequestInfo) record.get("RequetInfo");
+        RequestInfo requestInfo = (RequestInfo) record.get("RequestInfo");
 
         if (processInstances != null && !processInstances.isEmpty()) {
             Map<String, Object> processInstance = processInstances.get(0);
