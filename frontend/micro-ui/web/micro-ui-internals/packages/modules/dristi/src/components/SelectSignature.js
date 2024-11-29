865d9b09e61500ceff04077cfeab4784ca0e517b
nitish-beehyv <nitish.k@beehyv.com>
2024-11-29 12:31:17 +0530

    E sign enabled prod and change redirect link (#1224)
    * e-sign disable removed
    
    * change e sign url

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SelectSignature.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SelectSignature.js
@@ -98,7 +98,7 @@ function SelectSignature({ t, config, onSelect, formData = {}, errors }) {
         localStorage.setItem("eSignWindowObject", JSON.stringify(eSignData));
         const form = document.createElement("form");
         form.method = "POST";
-        form.action = "https://es-staging.cdac.in/esignlevel1/2.1/form/signdoc";
+        form.action = "https://esignservice.cdac.in/esign2.1/2.1/form/signdoc";
         const eSignRequestInput = document.createElement("input");
         eSignRequestInput.type = "hidden";
         eSignRequestInput.name = "eSignRequest";
