72b75d1ab7adf3554090b050a45775e2794ce568
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2024-12-13 11:37:10 +0530

    Openapi changes (#1325) (#1326)
    * Openapi changes (#1325)
    
    * openapi related changes
    
    * updated openapis
    
    * updated openapis
    
    * added test cases
    
    * removed un necessary lines
    
    * removed un necessary lines

--- backend/case/src/main/java/org/pucar/dristi/web/models/CaseListLineItem.java
+++ backend/case/src/main/java/org/pucar/dristi/web/models/CaseListLineItem.java
@@ -1,18 +1,20 @@
 package org.pucar.dristi.web.models;
 
-
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.egov.common.contract.response.ResponseInfo;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
 
 /**
- * CaseSummaryResponse
+ * CaseListLineItem
  */
 @Validated
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-12-03T13:11:23.212020900+05:30[Asia/Calcutta]")
@@ -20,16 +22,14 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CaseSummaryResponse {
-    @JsonProperty("ResponseInfo")
+public class CaseListLineItem {
+    @JsonProperty("caseNumber")
 
-    @Valid
-    private ResponseInfo responseInfo = null;
+    private String caseNumber = null;
 
-    @JsonProperty("caseSummary")
+    @JsonProperty("caseTitle")
 
-    @Valid
-    private CaseSummary caseSummary = null;
+    private String caseTitle = null;
 
 
 }
