986e37bf3876d3f8419cc2981d18882b59ba4a10
Nitish Kumar <nitish.k@beehyv.com>
2024-12-16 16:30:16 +0530

    Fix css of void status, fix handle cancel issue and change hading for unmark modal

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/AdmittedCases/AdmittedCase.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/AdmittedCases/AdmittedCase.js
@@ -859,7 +859,12 @@ const AdmittedCases = () => {
     return {
       handleClose: handleClose,
       heading: {
-        label: "view_reason_for_voiding" === documentSubmission?.[0]?.itemType ? t("REASON_FOR_VOIDING") : t("ARE_YOU_SURE_TO_MARK_AS_VOID"),
+        label:
+          "view_reason_for_voiding" === documentSubmission?.[0]?.itemType
+            ? t("REASON_FOR_VOIDING")
+            : "unmark_void_submission" === documentSubmission?.[0]?.itemType
+            ? t("ARE_YOU_SURE_TO_UNMARK_AS_VOID")
+            : t("ARE_YOU_SURE_TO_MARK_AS_VOID"),
       },
       isStepperModal: true,
       actionSaveLabel:
@@ -871,9 +876,9 @@ const AdmittedCases = () => {
           ? t("MARK_VOID_CONFIRM")
           : t("MARK_AS_VOID"),
       actionCancelLabel: t("MARK_VOID_CANCEL"),
-      actionCancelOnSubmit: handleClose,
       steps: [
         {
+          actionCancelOnSubmit: handleClose,
           actionSaveLableType: "mark_as_void" === documentSubmission?.[0]?.itemType ? "WARNING" : null,
           modalBody: (
             <VoidSubmissionBody
