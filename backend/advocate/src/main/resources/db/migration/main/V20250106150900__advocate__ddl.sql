b873a7c374baf1ff48eb55d091623b48115a3c4d
radheshjoshi1 <radhesh.joshi@beehyv.com>
2025-01-06 15:37:44 +0530

    tenantId index added for advocate

---
+++ backend/advocate/src/main/resources/db/migration/main/V20250106150900__advocate__ddl.sql
@@ -0,0 +1,4 @@
+CREATE INDEX IF NOT EXISTS idx_dristi_advocate_tenant_id ON dristi_advocate(tenantId);
+
+CREATE INDEX IF NOT EXISTS idx_dristi_advocate_clerk_tenant_id ON dristi_advocate_clerk(tenantId);
+
