b75274ad06085e5f0f4e0832651af7b8fca7cf95
radheshjoshi1 <radhesh.joshi@beehyv.com>
2025-01-07 12:25:20 +0530

    added concurrently

--- backend/hearing/src/main/resources/db/migration/main/V20250106151900__hearing__ddl.sql
+++ backend/hearing/src/main/resources/db/migration/main/V20250106151900__hearing__ddl.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS idx_dristi_hearing_tenant_id ON dristi_hearing (tenantId);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_dristi_hearing_tenant_id ON dristi_hearing (tenantId);
