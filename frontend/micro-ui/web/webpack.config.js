84a8f19d79ea6dda647de5e7c58a49e672926c8f
utpal.kant <utpal.kant@beehyv.com>
2025-02-19 19:07:51 +0530

    hash bundle output for cache-busting

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name]-[contenthash].bundle.js",
     path: path.resolve(__dirname, "build"),
     publicPath: "/digit-ui/",
   },
