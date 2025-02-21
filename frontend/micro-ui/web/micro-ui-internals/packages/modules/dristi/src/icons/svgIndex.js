36eec0be8d9bcf60851249ce33e397152d06ba6a
Nitish Kumar <nitish.k@beehyv.com>
2025-02-21 17:12:42 +0530

    make the disable checkbox color to  darker gray and fill it with same color #3222

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/icons/svgIndex.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/icons/svgIndex.js
@@ -1609,10 +1609,10 @@ const ForwardArrow = () => {
   );
 };
 
-const CheckSvg = ({ className, style = {} }) => (
+const CheckSvg = ({ className, style = {}, checkBoxFill = "none", tickStyle = {} }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={COLOR_FILL} className={className} style={style}>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+    <path d="M0 0h24v24H0z" fill={checkBoxFill} />
+    <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" style={tickStyle} />
   </svg>
 );
 
