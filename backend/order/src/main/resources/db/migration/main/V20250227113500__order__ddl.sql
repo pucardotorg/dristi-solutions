c2bba3ac38c7451b476a1328f74e5f8173210704
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-02-27 23:44:17 +0530

    updated  gin index syntax (#1976)

--- backend/order/src/main/resources/db/migration/main/V20250227113500__order__ddl.sql
+++ backend/order/src/main/resources/db/migration/main/V20250227113500__order__ddl.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS idx_dristi_order_composite_items dristi_orders USING GIN (compositeItems);
+CREATE INDEX IF NOT EXISTS idx_dristi_order_composite_items ON dristi_orders USING GIN (compositeItems);
