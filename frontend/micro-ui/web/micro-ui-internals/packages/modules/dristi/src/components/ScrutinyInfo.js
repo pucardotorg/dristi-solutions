c05744d88856e6f72cc6505af50c57d1ebd02788
nitish-beehyv <nitish.k@beehyv.com>
2025-03-04 23:02:15 +0530

    resolved frontend merge conflict from main to develop on march 4 (#2031)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/ScrutinyInfo.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/ScrutinyInfo.js
@@ -3,10 +3,10 @@ import { FSOErrorIcon } from "../icons/svgIndex";
 
 function ScrutinyInfo({ config, t }) {
   return (
-    <div style={{ backgroundColor: "#fce8e8", marginBottom: 8, padding: 6, borderRadius: 5 }}>
+    <div style={{ backgroundColor: config?.populators?.isWarning ? "#FDF2DE" : "#fce8e8", marginBottom: 8, padding: 6, borderRadius: 5 }}>
       <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center", gap: "10px", marginBottom: 8 }}>
-        <FSOErrorIcon />
-        <div style={{ fontWeight: 700 }}>{t("CS_FSO_MARKED_ERROR")}</div>
+        <FSOErrorIcon color={config?.populators?.isWarning ? "#443627" : "#BB2C2F"} />
+        <div style={{ fontWeight: 700 }}>{config?.populators?.isWarning ? t("WARNING") : t("CS_FSO_MARKED_ERROR")}</div>
       </div>
       {t(config.populators.scrutinyMessage)}
     </div>
