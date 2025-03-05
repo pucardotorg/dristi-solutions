e75bd459ce2e62ee699b0fa82b330089e56c84a8
ashutosh <ashutosh.singh@beehyv.com>
2025-03-05 16:59:30 +0530

    [bug-fix] #3350

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/ProfileComponent.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+import { EditProfileIcon, SelectLanguage, LogoutIcon, TriangleIcon } from "../../../../dristi/src/icons/svgIndex";
+import ChangeLanguage from "@egovernments/digit-ui-module-core/src/components/ChangeLanguage";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+
+const ProfileComponent = ({ userDetails, userOptions, handleUserDropdownSelection, profilePic, TextToImg, onLogout }) => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const [showModal, setShowModal] = useState(false);
+  console.log("userDetails :>> ", userDetails);
+  const isEmployee = window.location.href.includes("/employee");
+  const showProfilePage = () => {
+    const redirectUrl = isEmployee
+      ? `/${window?.contextPath}/employee/dristi/home/edit-profile`
+      : `/${window?.contextPath}/citizen/dristi/home/edit-profile`;
+    history.push(redirectUrl);
+    setShowModal(false);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleClickOutside = (event) => {
+    if (event.target.closest(".profile-component") === null) {
+      setShowModal(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showModal) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showModal]);
+
+  const handleSelection = (option) => {
+    if (option.name === "Logout") {
+      onLogout();
+    } else {
+      showProfilePage();
+    }
+    setShowModal(false);
+  };
+
+  const userRole = userDetails?.info?.roles?.some((role) => role.name === "ADVOCATE_ROLE") ? t("ADVOCATE") : t("LITIGANT_OPT");
+
+  return (
+    <div className="profile-component">
+      <div onClick={toggleModal}>
+        {profilePic == null ? (
+          <TextToImg name={userDetails?.info?.name || userDetails?.info?.userInfo?.name || "Citizen"} />
+        ) : (
+          <img src={profilePic} alt="profile" />
+        )}
+      </div>
+      {showModal && (
+        <div className="profile-modal">
+          <div className="triangle-icon">
+            <TriangleIcon />
+          </div>
+          <div className="profile-header">
+            <div className="profile-name">{userDetails?.info?.name}</div>
+            <div className="user-type">
+              {userRole} | {userDetails?.info?.mobileNumber}
+            </div>
+          </div>
+          <div className="profile-options">
+            <div className="edit-profile" onClick={() => handleSelection({ name: "Edit Profile" })}>
+              <span role="img" aria-label="edit" s className="edit-profile-icon">
+                <EditProfileIcon />
+              </span>
+              {t("EDIT_PROFILE")}
+            </div>
+            <div className="language-selection">
+              <div className="language-select-icon">
+                <span role="img" aria-label="language" className="language-icon">
+                  <SelectLanguage />
+                </span>
+                {t("CS_COMMON_LANGUAGE")}
+              </div>
+              {<ChangeLanguage isProfileComponent={true} dropdown={true} dropdownClassName="cp-class" />}
+            </div>
+          </div>
+          <div className="logout-options">
+            <div className="logout" onClick={() => handleSelection({ name: "Logout" })}>
+              <div className="logout-icon-text">
+                <span className="logout-icon" role="img" aria-label="language">
+                  <LogoutIcon />
+                </span>
+                {t("CORE_COMMON_LOGOUT")}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+ProfileComponent.propTypes = {
+  userDetails: PropTypes.object.isRequired,
+  userOptions: PropTypes.array.isRequired,
+  handleUserDropdownSelection: PropTypes.func.isRequired,
+  profilePic: PropTypes.string,
+  TextToImg: PropTypes.func.isRequired,
+};
+
+ProfileComponent.defaultProps = {
+  profilePic: null,
+};
+
+export default ProfileComponent;
