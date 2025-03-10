78a9aed6124dcb15050d07abc23c503aeb2c1afc
radheshjoshi1 <radhesh.joshi@beehyv.com>
2025-03-10 21:15:28 +0530

    handle bulk reschedule for update hearing index

--- backend/transformer/src/main/java/org/egov/transformer/consumer/HearingConsumer.java
+++ backend/transformer/src/main/java/org/egov/transformer/consumer/HearingConsumer.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.egov.transformer.config.TransformerProperties;
 import org.egov.transformer.models.Hearing;
+import org.egov.transformer.models.HearingBulkRequest;
 import org.egov.transformer.models.HearingRequest;
 import org.egov.transformer.service.HearingService;
 import org.slf4j.Logger;
@@ -15,6 +16,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -47,7 +50,22 @@ public class HearingConsumer {
     @KafkaListener(topics = {"${transformer.consumer.update.start.end.time.topic}"})
     public void updateStartEndTime(ConsumerRecord<String, Object> payload,
                               @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-        publishHearing(payload, transformerProperties.getUpdateHearingTopic());
+        publishBulkHearing(payload, transformerProperties.getUpdateHearingTopic());
+    }
+    private void publishBulkHearing(ConsumerRecord<String, Object> payload,
+                                    @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        try {
+            HearingBulkRequest bulkRequest = objectMapper.readValue(payload.value().toString(), HearingBulkRequest.class);
+            List<Hearing> hearings = bulkRequest.getHearing();
+            hearings.forEach(hearing -> {
+                HearingRequest hearingRequest = HearingRequest.builder()
+                        .requestInfo(bulkRequest.getRequestInfo())
+                        .hearing(hearing).build();
+                hearingService.enrichOpenHearings(hearingRequest);
+            });
+        } catch (Exception exception) {
+            logger.error("error in saving hearing", exception);
+        }
     }
 
     @KafkaListener(topics = {"${transformer.bulk.reschedule.topic}"})
