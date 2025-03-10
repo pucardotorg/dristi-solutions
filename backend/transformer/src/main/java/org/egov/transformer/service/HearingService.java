5e7552e863b78d4fef04451f15a227523cbd7e35
radheshjoshi1 <167431111+radheshjoshi1@users.noreply.github.com>
2025-03-10 09:50:11 +0530

    enhance transformer service for open hearings (#2097)
    * enhance transformer service for open hearings
    
    * add service call for case instead es
    
    * add service call for case instead es
    
    * fix consumer
    
    * add supoort file

--- backend/transformer/src/main/java/org/egov/transformer/service/HearingService.java
+++ backend/transformer/src/main/java/org/egov/transformer/service/HearingService.java
@@ -1,15 +1,21 @@
 package org.egov.transformer.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.egov.common.contract.request.RequestInfo;
 import org.egov.transformer.config.TransformerProperties;
 import org.egov.transformer.models.CourtCase;
 import org.egov.transformer.models.Hearing;
 import org.egov.transformer.models.HearingRequest;
+import org.egov.transformer.models.OpenHearing;
 import org.egov.transformer.producer.TransformerProducer;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import static org.egov.transformer.config.ServiceConstants.*;
 
 @Slf4j
 @Service
@@ -37,8 +43,34 @@ public class HearingService {
 
         HearingRequest hearingRequest = new HearingRequest();
         hearingRequest.setHearing(hearing);
-
         producer.push(properties.getSaveHearingTopic(), hearingRequest);
     }
 
+    public void enrichOpenHearings(HearingRequest hearingRequest) {
+        Hearing hearing = hearingRequest.getHearing();
+        RequestInfo requestInfo = hearingRequest.getRequestInfo();
+        CourtCase courtCase = caseService.getCase(hearing.getFilingNumber().get(0), hearing.getTenantId(), requestInfo);
+        if(!Objects.equals(hearing.getStatus(), HEARD) &&
+                !Objects.equals(hearing.getStatus(), ADJOURNED) &&
+                !Objects.equals(hearing.getStatus(), ABATED) &&
+                !Objects.equals(hearing.getStatus(), CLOSED)) {
+            OpenHearing openHearing = getOpenHearing(hearing, courtCase);
+            producer.push(properties.getOpenHearingTopic(), openHearing);
+        }
+    }
+
+    @NotNull
+    private static OpenHearing getOpenHearing(Hearing hearing, CourtCase courtCase) {
+        OpenHearing openHearing = new OpenHearing();
+        openHearing.setHearingUuid(hearing.getId().toString());
+        openHearing.setHearingNumber(hearing.getHearingId());
+        openHearing.setFilingNumber(hearing.getFilingNumber().get(0));
+        openHearing.setCaseTitle(courtCase.getCaseTitle());
+        openHearing.setCaseNumber(hearing.getCaseReferenceNumber());
+        openHearing.setStage(courtCase.getStage());
+        openHearing.setCaseUuid(courtCase.getId().toString());
+        openHearing.setStatus(hearing.getStatus());
+        openHearing.setTenantId(hearing.getTenantId());
+        return openHearing;
+    }
 }
