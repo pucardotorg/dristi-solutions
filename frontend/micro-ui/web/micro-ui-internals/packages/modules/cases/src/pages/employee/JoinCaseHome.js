23cdb2ae475d46743bebffd8e0b9b2e76cec018d
utpalBeehyv <utpal.kant@beehyv.com>
2025-03-11 18:24:31 +0530

    Fix for showing accused data in complaintTab (#2171)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/cases/src/pages/employee/JoinCaseHome.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/cases/src/pages/employee/JoinCaseHome.js
@@ -835,10 +835,10 @@ const JoinCaseHome = ({ refreshInbox, setShowSubmitResponseModal, setResponsePen
                         vakalatnamaFileUpload: {
                           document: [
                             {
-                              documentType: party?.uploadedVakalatnama?.documentType,
-                              fileStore: party?.uploadedVakalatnama?.fileStore,
-                              documentName: party?.uploadedVakalatnama?.documentName,
-                              fileName: party?.uploadedVakalatnama?.fileName,
+                              documentType: matchedUser?.uploadedVakalatnama?.documentType,
+                              fileStore: matchedUser?.uploadedVakalatnama?.fileStore,
+                              documentName: matchedUser?.uploadedVakalatnama?.documentName,
+                              fileName: matchedUser?.uploadedVakalatnama?.fileName,
                             },
                           ],
                         },
@@ -848,10 +848,10 @@ const JoinCaseHome = ({ refreshInbox, setShowSubmitResponseModal, setResponsePen
                           document: [
                             ...(formdataItem?.data?.multipleAdvocatesAndPip?.vakalatnamaFileUpload?.document || []),
                             {
-                              documentType: party?.uploadedVakalatnama?.documentType,
-                              fileStore: party?.uploadedVakalatnama?.fileStore,
-                              documentName: party?.uploadedVakalatnama?.documentName,
-                              fileName: party?.uploadedVakalatnama?.fileName,
+                              documentType: matchedUser?.uploadedVakalatnama?.documentType,
+                              fileStore: matchedUser?.uploadedVakalatnama?.fileStore,
+                              documentName: matchedUser?.uploadedVakalatnama?.documentName,
+                              fileName: matchedUser?.uploadedVakalatnama?.fileName,
                             },
                           ],
                         },
@@ -1786,7 +1786,11 @@ const JoinCaseHome = ({ refreshInbox, setShowSubmitResponseModal, setResponsePen
               additionalDetails: {
                 ...caseDetails?.additionalDetails,
                 ...(selectPartyData?.partyInvolve?.value !== "RESPONDENTS" && {
-                  advocateDetails: getAdvocatesDetails(caseDetails?.additionalDetails?.advocateDetails, documentUploadResult),
+                  advocateDetails: getAdvocatesDetails(
+                    caseDetails?.additionalDetails?.advocateDetails,
+                    documentUploadResult,
+                    selectPartyData?.isReplaceAdvocate
+                  ),
                 }),
                 ...(selectPartyData?.partyInvolve?.value === "RESPONDENTS" && {
                   respondentDetails,
