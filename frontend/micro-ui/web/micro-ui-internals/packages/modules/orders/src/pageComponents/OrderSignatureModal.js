d4b88465771f3a7a572505abed4fbca6e40de8f6
utpalBeehyv <utpal.kant@beehyv.com>
2025-03-21 12:23:52 +0530

    Correct props passing (#2300)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pageComponents/OrderSignatureModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pageComponents/OrderSignatureModal.js
@@ -28,7 +28,7 @@ function OrderSignatureModal({
   saveOnsubmitLabel,
   setSignedDocumentUploadID,
   orderPdfFileStoreID,
-  businessOfTheDay
+  businessOfDay
 }) {
   const [isSigned, setIsSigned] = useState(false);
   const { handleEsign, checkSignStatus } = useESign();
@@ -131,7 +131,7 @@ function OrderSignatureModal({
                   // setOpenAadharModal(true);
                   // setIsSigned(true);
                   localStorage.setItem("orderPDF", orderPdfFileStoreID);
-                  localStorage.setItem("businessOfTheDay", businessOfTheDay);
+                  localStorage.setItem("businessOfTheDay", businessOfDay);
                   handleEsign(name, pageModule, orderPdfFileStoreID, judgePlaceholder);
                 }}
                 className={"aadhar-sign-in"}
