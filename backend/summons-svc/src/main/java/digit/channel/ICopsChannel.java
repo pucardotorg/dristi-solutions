c0bbe6bd2dc3d66ddba8a718e7a8d8e4ca5f0a3f
radheshjoshi1 <radhesh.joshi@beehyv.com>
2025-04-15 22:01:59 +0530

    update doc sub type for icops

--- backend/summons-svc/src/main/java/digit/channel/ICopsChannel.java
+++ backend/summons-svc/src/main/java/digit/channel/ICopsChannel.java
@@ -10,6 +10,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import digit.config.ServiceConstants.*;
+
+import static digit.config.ServiceConstants.*;
 
 @Component
 @Slf4j
@@ -27,6 +30,7 @@ public class ICopsChannel implements ExternalChannel {
 
     @Override
     public ChannelMessage sendSummons(TaskRequest request) {
+        updateDocSubType(request);
         StringBuilder uri = new StringBuilder();
         uri.append(config.getICopsHost())
                 .append(config.getICopsRequestEndPoint());
@@ -38,5 +42,19 @@ public class ICopsChannel implements ExternalChannel {
                 requestEntity, ChannelResponse.class);
         log.info("Response Body: {}", responseEntity.getBody());
         return responseEntity.getBody().getChannelMessage();
+    }
+
+    private void updateDocSubType(TaskRequest request) {
+        Task task = request.getTask();
+        if(task.getTaskType().equals(SUMMON)) {
+            if(task.getTaskDetails().getSummonDetails().getDocSubType().equals(ACCUSED)) {
+                task.getTaskDetails().getSummonDetails().setDocSubType(SUMMON_TO_ACCUSED);
+
+            } else if(task.getTaskDetails().getSummonDetails().getSummonId().equals(WITNESS)) {
+                task.getTaskDetails().getSummonDetails().setDocSubType(SUMMON_TO_WITNESS);
+            }
+        } else if(task.getTaskType().equals(WARRANT)) {
+                task.getTaskDetails().getWarrantDetails().setDocSubType(WARRANT_TO_ACCUSED);
+        }
     }
 }
