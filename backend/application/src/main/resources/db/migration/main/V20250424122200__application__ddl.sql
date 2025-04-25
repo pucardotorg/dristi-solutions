7ef5309759781d43997c872f3b2006a0280abf38
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-04-25 14:31:12 +0530

    added index

--- backend/application/src/main/resources/db/migration/main/V20250424122200__application__ddl.sql
+++ backend/application/src/main/resources/db/migration/main/V20250424122200__application__ddl.sql
@@ -1,6 +1,8 @@
 ALTER TABLE dristi_application
 ADD COLUMN courtId VARCHAR(64);
 
+CREATE INDEX idx_dristi_application_courtid ON dristi_application(courtId);
+
 UPDATE dristi_application
 SET courtId = 'KLKM52'
 WHERE courtId IS NULL;
--- backend/task/src/main/resources/db/migration/main/V20250422153200__task__ddl.sql
+++ backend/task/src/main/resources/db/migration/main/V20250422153200__task__ddl.sql
@@ -1,6 +1,8 @@
 ALTER TABLE dristi_task
 ADD COLUMN courtId VARCHAR(64);
 
+CREATE INDEX idx_dristi_task_courtid ON dristi_task(courtId);
+
 UPDATE dristi_task
 SET courtId = 'KLKM52'
 WHERE courtId IS NULL;
