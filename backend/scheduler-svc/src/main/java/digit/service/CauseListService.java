cd1c822baa11794d2200c9b827d9b374f93a9909
i-m-dhruv-gupta <155708531+i-m-dhruv-gupta@users.noreply.github.com>
2025-05-05 20:23:28 +0530

    Timezone for now fixed (#2916)

--- backend/scheduler-svc/src/main/java/digit/service/CauseListService.java
+++ backend/scheduler-svc/src/main/java/digit/service/CauseListService.java
@@ -763,7 +763,7 @@ public class CauseListService {
         LocalDate today = LocalDate.now(ZoneId.of("Asia/Kolkata"));
         LocalDate yesterday = today.minusDays(1);
         LocalDate tomorrow = today.plusDays(1);
-        LocalTime now = LocalTime.now();
+        LocalTime now = LocalTime.now(ZoneId.of("Asia/Kolkata"));
         String courtId = recentCauseListSearchCriteria.getCourtId();
 
         criteriaList.add(CauseListSearchCriteria.builder().searchDate(today).courtId(courtId).build());
