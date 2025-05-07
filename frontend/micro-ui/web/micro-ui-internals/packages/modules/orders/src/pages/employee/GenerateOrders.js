ce60a907b3d29ecbe7db370a98662a93235f026b
utpal.kant <utpal.kant@beehyv.com>
2025-05-07 19:06:31 +0530

    update the ui screen to respective figma

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pages/employee/GenerateOrders.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pages/employee/GenerateOrders.js
@@ -4450,6 +4450,12 @@ const GenerateOrders = () => {
             hasError = true;
             break;
           }
+          
+          if (formData?.warrantFor?.selectedChannels?.length === 0) {
+            setShowErrorToast({ label: t("PLESE_SELECT_ADDRESSS"), error: true });
+            hasError = true;
+            break;
+          }
 
           if (
             formData?.warrantFor?.selectedChannels?.some(
