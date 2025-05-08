187ee0f74839080d5121ba50f0241ef00a0496ed
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-05-09 00:34:13 +0530

    added database source config for read and write operations (#2822) (#2972)
    * added database source config for read and write operations
    
    * updated the configuration to resolve the build errors
    
    * updated refactor comments
    
    * added database source config for read and (#2844)
    
    write operations
    
    ---------
    
    Co-authored-by: manimaarans <110804183+manimaarans@users.noreply.github.com>

--- backend/casemanagement/src/main/java/org/pucar/dristi/repository/CaseBundleRepository.java
+++ backend/casemanagement/src/main/java/org/pucar/dristi/repository/CaseBundleRepository.java
@@ -3,6 +3,7 @@ package org.pucar.dristi.repository;
 
 import org.pucar.dristi.web.models.BulkCaseBundleTracker;
 import org.pucar.dristi.web.models.CaseBundleTracker;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,18 +11,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CaseBundleRepository {
 
-
-    private  final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate writerJdbcTemplate;
 
     @Autowired
-    public CaseBundleRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public CaseBundleRepository(@Qualifier("writerJdbcTemplate") JdbcTemplate writerJdbcTemplate) {
+        this.writerJdbcTemplate = writerJdbcTemplate;
     }
     public void insertCaseTracker(CaseBundleTracker caseBundleTracker) {
         String sql = "INSERT INTO case_bundle_tracker (id, startTime, endTime, pageCount, createdBy, lastModifiedBy, createdTime, lastModifiedTime) " +
                 "VALUES (?, ?, ?, ?, ?, ?,  ?, ?)";
 
-        jdbcTemplate.update(sql, caseBundleTracker.getId(), caseBundleTracker.getStartTime(), caseBundleTracker.getEndTime(),
+        writerJdbcTemplate.update(sql, caseBundleTracker.getId(), caseBundleTracker.getStartTime(), caseBundleTracker.getEndTime(),
                 caseBundleTracker.getPageCount(), caseBundleTracker.getAuditDetails().getCreatedBy(), caseBundleTracker.getAuditDetails().getLastModifiedBy()
                 , caseBundleTracker.getAuditDetails().getCreatedTime(), caseBundleTracker.getAuditDetails().getLastModifiedTime());
     }
@@ -30,7 +30,7 @@ public class CaseBundleRepository {
         String sql = "INSERT INTO case_bundle_bulk_tracker (id, startTime, endTime, caseCount) " +
                 "VALUES (?, ?, ?, ?)";
 
-        jdbcTemplate.update(sql, bulkCaseBundleTracker.getId(), bulkCaseBundleTracker.getStartTime(), bulkCaseBundleTracker.getEndTime(),
+        writerJdbcTemplate.update(sql, bulkCaseBundleTracker.getId(), bulkCaseBundleTracker.getStartTime(), bulkCaseBundleTracker.getEndTime(),
                 bulkCaseBundleTracker.getCaseCount());
     }
 
--- backend/hearing/src/main/java/org/pucar/dristi/Main.java
+++ backend/hearing/src/main/java/org/pucar/dristi/Main.java
@@ -4,12 +4,14 @@ package org.pucar.dristi;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @Import({ TracerConfiguration.class })
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.pucar.dristi", "org.pucar.dristi.web.controllers" , "org.pucar.dristi.config"})
+@EnableConfigurationProperties
 public class Main {
 
 
