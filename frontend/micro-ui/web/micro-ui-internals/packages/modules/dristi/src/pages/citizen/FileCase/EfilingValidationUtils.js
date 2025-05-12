6dd189f0882dcba71a8361f03076326116d6759b
maruthiPhanendra <phanendra.chaluvadi@beehyv.com>
2025-05-12 13:42:20 +0530

    adding document id in old documents

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/EfilingValidationUtils.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/EfilingValidationUtils.js
@@ -3043,6 +3043,13 @@ export const updateCaseDetails = async ({
   if (isCaseSignedState && action === "SUBMIT_CASE") {
     return null;
   }
+  const updatedTempDocList = tempDocList?.map((doc) => {
+    const existingDoc = caseDetails?.documents?.find((existingDoc) => existingDoc?.fileStore === doc?.fileStore);
+    if (existingDoc) {
+      return { ...doc, id: existingDoc?.id };
+    }
+    return doc;
+  });
 
   return await DRISTIService.caseUpdateService(
     {
@@ -3051,7 +3058,7 @@ export const updateCaseDetails = async ({
         caseTitle,
         litigants: !caseDetails?.litigants ? [] : caseDetails?.litigants,
         ...data,
-        documents: tempDocList,
+        documents: updatedTempDocList,
         advocateCount:
           formdata?.[0]?.data?.numberOfAdvocate || caseDetails?.additionalDetails?.advocateDetails?.formdata[0]?.data?.numberOfAdvocate || 0,
         linkedCases: caseDetails?.linkedCases ? caseDetails?.linkedCases : [],
