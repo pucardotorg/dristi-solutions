8d90397bbe3d575c2ab61cde905a6a25815c627d
maruthiPhanendra <phanendra.chaluvadi@beehyv.com>
2025-05-15 15:57:43 +0530

    creating new filestore for old litigants

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/VerifyPhoneNumber.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/VerifyPhoneNumber.js
@@ -9,6 +9,7 @@ import Button from "./Button";
 import Modal from "./Modal";
 import OTPInput from "./OTPInput";
 import { maskEmail } from "../Utils";
+import useNewFileStoreId from "../hooks/dristi/useNewFilestoreId";
 const TYPE_REGISTER = { type: "register" };
 const TYPE_LOGIN = { type: "login" };
 const DEFAULT_USER = "digit-user";
@@ -207,6 +208,8 @@ function VerifyPhoneNumber({ t, config, onSelect, formData = {}, errors, setErro
     }
   };
 
+  const { newFilestore } = useNewFileStoreId();
+
   const searchIndividualUser = (info, tokens) => {
     DRISTIService.searchIndividualUser(
       {
@@ -216,7 +219,7 @@ function VerifyPhoneNumber({ t, config, onSelect, formData = {}, errors, setErro
       },
       { tenantId: stateCode, limit: 10, offset: 0 }
     )
-      .then((individualData) => {
+      .then(async (individualData) => {
         if (Array.isArray(individualData?.Individual) && individualData?.Individual?.length > 0) {
           const addressLine1 = individualData?.Individual?.[0]?.address[0]?.addressLine1 || "Telangana";
           const addressLine2 = individualData?.Individual?.[0]?.address[0]?.addressLine2 || "Rangareddy";
@@ -236,7 +239,7 @@ function VerifyPhoneNumber({ t, config, onSelect, formData = {}, errors, setErro
           const givenName = individualData?.Individual?.[0]?.name?.givenName || "";
           const otherNames = individualData?.Individual?.[0]?.name?.otherNames || "";
           const familyName = individualData?.Individual?.[0]?.name?.familyName || "";
-
+          const newIndividualFilestoreId = await newFilestore(tenantId, identifierIdDetails?.fileStoreId || "", identifierIdDetails?.filename);
           const data = {
             "addressDetails-select": {
               pincode: pincode,
@@ -280,7 +283,7 @@ function VerifyPhoneNumber({ t, config, onSelect, formData = {}, errors, setErro
                 individualId: individualData?.Individual?.[0]?.individualId,
                 userUuid: individualData?.Individual?.[0]?.userUuid,
                 document: identifierIdDetails?.fileStoreId
-                  ? [{ fileName: idType, fileStore: identifierIdDetails?.fileStoreId, documentName: identifierIdDetails?.filename }]
+                  ? [{ fileName: idType, fileStore: newIndividualFilestoreId?.fileStoreId, documentName: identifierIdDetails?.filename }]
                   : null,
                 ...(config?.key === "poaVerification"
                   ? {
