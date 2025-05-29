50f2e0fca329d58dd1639ddbc91c6ef919a634ab
prasanna2400 <prasannajeet.v@beehyv.com>
2025-05-29 16:31:04 +0530

    enhance index search end point to support dynamic sorting

--- common/inbox/src/main/java/org/egov/inbox/util/MDMSUtil.java
+++ common/inbox/src/main/java/org/egov/inbox/util/MDMSUtil.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.common.utils.MultiStateInstanceUtil;
-import org.egov.inbox.web.model.InboxRequest;
+import org.egov.inbox.web.model.InboxSortConfiguration;
 import org.egov.inbox.web.model.V2.InboxQueryConfiguration;
 import org.egov.mdms.model.MasterDetail;
 import org.egov.mdms.model.MdmsCriteria;
@@ -43,39 +43,62 @@ public class MDMSUtil {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Cacheable(value="inboxConfiguration")
+    @Cacheable(value = "inboxConfiguration")
     public InboxQueryConfiguration getConfigFromMDMS(String tenantId, String moduleName) {
 
         StringBuilder uri = new StringBuilder();
         uri.append(mdmsHost).append(mdmsUrl);
-        MdmsCriteriaReq mdmsCriteriaReq = getMdmsRequestForInboxQueryConfiguration(tenantId);
+        MdmsCriteriaReq mdmsCriteriaReq = getMdmsRequestForInboxQueryConfiguration(tenantId, INBOX_QUERY_CONFIG_NAME, INBOX_MODULE_CODE);
         Object response = new HashMap<>();
         List<Map> configs;
         try {
             response = restTemplate.postForObject(uri.toString(), mdmsCriteriaReq, Map.class);
             String jsonpath = MDMS_RESPONSE_JSONPATH.replace(MODULE_PLACEHOLDER, moduleName);
             configs = JsonPath.read(response, jsonpath);
-        }catch(Exception e) {
-            throw new CustomException("CONFIG_ERROR","Error in fetching inbox query configuration from MDMS for: " + moduleName);
+        } catch (Exception e) {
+            throw new CustomException("CONFIG_ERROR", "Error in fetching inbox query configuration from MDMS for: " + moduleName);
         }
 
         if (CollectionUtils.isEmpty(configs))
-            throw new CustomException("CONFIG_ERROR","Inbox Query Configuration not found in MDMS response for: " + moduleName);
+            throw new CustomException("CONFIG_ERROR", "Inbox Query Configuration not found in MDMS response for: " + moduleName);
 
         InboxQueryConfiguration configuration = objectMapper.convertValue(configs.get(0), InboxQueryConfiguration.class);
 
         return configuration;
     }
 
-    private MdmsCriteriaReq getMdmsRequestForInboxQueryConfiguration(String tenantId) {
+    @Cacheable(value = "sortConfiguration")
+    public InboxSortConfiguration getSortConfigFromMDMS(String tenantId, String moduleName) {
+
+        StringBuilder uri = new StringBuilder();
+        uri.append(mdmsHost).append(mdmsUrl);
+        MdmsCriteriaReq mdmsCriteriaReq = getMdmsRequestForInboxQueryConfiguration(tenantId, INBOX_SORT_CONFIG_NAME, INBOX_MODULE_CODE);
+        Object response = new HashMap<>();
+        List<Map> configs;
+        try {
+            response = restTemplate.postForObject(uri.toString(), mdmsCriteriaReq, Map.class);
+            String jsonpath = MDMS_SORT_RESPONSE_JSONPATH.replace(MODULE_PLACEHOLDER, moduleName);
+            configs = JsonPath.read(response, jsonpath);
+        } catch (Exception e) {
+            throw new CustomException("CONFIG_ERROR", "Error in fetching inbox query configuration from MDMS for: " + moduleName);
+        }
+
+        if (CollectionUtils.isEmpty(configs))
+            return null;
+        InboxSortConfiguration configuration = objectMapper.convertValue(configs.get(0), InboxSortConfiguration.class);
+
+        return configuration;
+    }
+
+    private MdmsCriteriaReq getMdmsRequestForInboxQueryConfiguration(String tenantId, String masterName, String moduleName) {
         MasterDetail masterDetail = new MasterDetail();
-        masterDetail.setName(INBOX_QUERY_CONFIG_NAME);
+        masterDetail.setName(masterName);
         List<MasterDetail> masterDetailList = new ArrayList<>();
         masterDetailList.add(masterDetail);
 
         ModuleDetail moduleDetail = new ModuleDetail();
         moduleDetail.setMasterDetails(masterDetailList);
-        moduleDetail.setModuleName(INBOX_MODULE_CODE);
+        moduleDetail.setModuleName(moduleName);
         List<ModuleDetail> moduleDetailList = new ArrayList<>();
         moduleDetailList.add(moduleDetail);
 
