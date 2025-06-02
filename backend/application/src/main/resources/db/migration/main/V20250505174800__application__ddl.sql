a06471c81199fa22fec3d24bf3ce4cb07725556c
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-06-02 12:52:53 +0530

    delete duplicate flyway files

--- backend/application/src/main/resources/db/migration/main/V20250505174800__application__ddl.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE dristi_application_document
-ADD COLUMN  isActive bool DEFAULT TRUE;
--- backend/task/src/main/resources/db/migration/main/V20250422153200__task__ddl.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE dristi_task
-ADD COLUMN courtId VARCHAR(64);
-
-CREATE INDEX idx_dristi_task_courtid ON dristi_task(courtId);
-
-UPDATE dristi_task
-SET courtId = 'KLKM52'
-WHERE courtId IS NULL;
