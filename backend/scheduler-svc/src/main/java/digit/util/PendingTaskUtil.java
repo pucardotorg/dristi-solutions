9f08319984351e44dc917ae9c077074e24518140
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-06-04 14:04:07 +0530

    added actioncategory for schedule hearing pending task

--- backend/scheduler-svc/src/main/java/digit/util/PendingTaskUtil.java
+++ backend/scheduler-svc/src/main/java/digit/util/PendingTaskUtil.java
@@ -60,6 +60,7 @@ public class PendingTaskUtil {
         pendingTask.setName(PENDING_TASK_NAME);
         pendingTask.setReferenceId("MANUAL_"+reScheduleHearing.getRescheduledRequestId());
         pendingTask.setStatus(PENDING_TASK_STATUS);
+        pendingTask.setActionCategory(PENDING_TASK_ACTION_CATEGORY);
         pendingTask.setFilingNumber(reScheduleHearing.getCaseId());
         pendingTask.setAssignedRole(List.of("JUDGE_ROLE"));
         pendingTask.setScreenType(SCREEN_TYPE);
