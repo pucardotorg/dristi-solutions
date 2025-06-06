58b68d3ec704cc68d3de33f788fb3dc5bf8ede41
i-m-dhruv-gupta <155708531+i-m-dhruv-gupta@users.noreply.github.com>
2025-06-06 11:39:14 +0530

    Enhancement 3889 litigant profile editing main (#3339)
    * Complainant Profile Editing Changes

---
+++ backend/analytics/src/main/java/org/pucar/dristi/web/models/ReferenceEntityTypeNameMapping.java
@@ -0,0 +1,21 @@
+package org.pucar.dristi.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReferenceEntityTypeNameMapping {
+
+    @JsonProperty("referenceEntityType")
+    private String referenceEntityType;
+
+    @JsonProperty("pendingTaskName")
+    private String pendingTaskName;
+}
+
