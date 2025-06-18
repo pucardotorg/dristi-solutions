b939d407d5fe4e3b0211138ff969f5aecfe1cb7b
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-06-18 13:00:40 +0530

    updated script

--- support/v1.7.0/reason-document-script.sql
+++ support/v1.7.0/reason-document-script.sql
@@ -6,7 +6,12 @@ select * from dristi_evidence_artifact where artifacttype = 'REASON_DOCUMENT';
 
 // need to update the artifact type to REASON_DOCUMENT
 
-update dristi_evidence_artifact SET filingtype = 'DIRECT' where artifacttype = 'REASON_DOCUMENT';
+UPDATE dristi_evidence_artifact
+SET isvoid = false,filingtype = 'DIRECT',status='SUBMITTED'
+WHERE artifacttype = 'REASON_DOCUMENT';
 
+// need to check what should be source type with bhuvanyu
+
+and need to update the source type to either accussed or complainant based on the data
 
 
