393e5724af55125746b47767bffcb1f49b1b374a
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-06-23 14:52:45 +0530

    moved openapi changes

--- backend/openapi/src/main/java/org/pucar/dristi/web/models/IndexSearchCriteria.java
+++ backend/openapi/src/main/java/org/pucar/dristi/web/models/IndexSearchCriteria.java
@@ -2,7 +2,6 @@ package org.pucar.dristi.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import org.egov.inbox.web.model.Criteria;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
@@ -20,18 +19,6 @@ public class IndexSearchCriteria {
 
     @JsonProperty("moduleSearchCriteria")
     private HashMap<String,Object> moduleSearchCriteria;
-
-    @JsonProperty("searchReviewProcess")
-    private Criteria searchReviewProcess;
-
-    @JsonProperty("searchViewApplication")
-    private Criteria searchViewApplication;
-
-    @JsonProperty("searchScheduleHearing")
-    private Criteria searchScheduleHearing;
-
-    @JsonProperty("searchRegisterCases")
-    private Criteria searchRegisterCases;
 
     @JsonProperty("offset")
     private Integer offset;
