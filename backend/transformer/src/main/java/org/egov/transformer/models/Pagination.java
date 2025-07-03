1a0e8b84706775e2bdee207415f49b135a307b68
prasanna2400 <prasannajeet.v@beehyv.com>
2025-07-03 14:21:07 +0530

    Update criterion for enriching nextHearingDate and hearingType in caseSearch

--- backend/transformer/src/main/java/org/egov/transformer/models/Pagination.java
+++ backend/transformer/src/main/java/org/egov/transformer/models/Pagination.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.egov.transformer.models.inbox.Order;
 import org.springframework.validation.annotation.Validated;
 
 /**
