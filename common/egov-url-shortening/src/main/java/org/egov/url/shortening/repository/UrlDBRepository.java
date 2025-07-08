e3a55646e4bd92f674e97a6716e2da15ffc173de
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-07-08 19:40:27 +0530

    added expiry logic

--- common/egov-url-shortening/src/main/java/org/egov/url/shortening/repository/UrlDBRepository.java
+++ common/egov-url-shortening/src/main/java/org/egov/url/shortening/repository/UrlDBRepository.java
@@ -2,11 +2,13 @@ package org.egov.url.shortening.repository;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.egov.url.shortening.model.ShortenRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.stereotype.Repository;
@@ -40,8 +42,8 @@ public class UrlDBRepository implements URLRepository{
     public void saveUrl(String key, ShortenRequest shortenRequest) {
 
     	String query = "INSERT INTO eg_url_shortener "
-    			+ "(id,validform,validto,url) "
-    			+ "values (?,?,?,?)";
+    			+ "(id,validfrom,validto,url,reference_id) "
+    			+ "values (?,?,?,?,?)";
     	log.info("Saving: {} at {}", shortenRequest.getUrl(), key);
         Boolean b = jdbcTemplate.execute(query,new PreparedStatementCallback<Boolean>(){  
             @Override  
@@ -50,8 +52,9 @@ public class UrlDBRepository implements URLRepository{
                       
                 ps.setString(1,key);
                 ps.setObject(2,shortenRequest.getValidFrom());
-                ps.setObject(3,shortenRequest.getValidTill());
-                ps.setString(4,shortenRequest.getUrl());              
+                ps.setObject(3,shortenRequest.getValidTo());
+                ps.setString(4,shortenRequest.getUrl());
+                ps.setString(5,shortenRequest.getReferenceId());
                 return ps.execute();  
                       
             }  
@@ -72,5 +75,42 @@ public class UrlDBRepository implements URLRepository{
         }
         return url;
     }
+
+    @Override
+    public ShortenRequest getShortenRequestByReferenceId(String referenceId) {
+        String query = "SELECT * FROM EG_URL_SHORTENER WHERE reference_id = ?";
+
+        List<ShortenRequest> results = jdbcTemplate.query(query, new BeanPropertyRowMapper<>(ShortenRequest.class), referenceId);
+
+        if (results.isEmpty()) {
+            return null;
+        } else {
+            ShortenRequest shortenRequest = results.get(0);
+            log.info("Retrieved {} at {}", shortenRequest.getUrl(), referenceId);
+            return shortenRequest;
+        }
+    }
+
+    @Override
+    public ShortenRequest getShortenRequestById(Long id) throws Exception {
+        String query = "SELECT * FROM EG_URL_SHORTENER WHERE id = ?";
+
+        String strprepStmtArgs = "url:"+id;
+
+        List<ShortenRequest> results = jdbcTemplate.query(query, new BeanPropertyRowMapper<>(ShortenRequest.class), strprepStmtArgs);
+
+        if (results.isEmpty()) {
+            return null;
+        } else {
+            return results.get(0);
+        }
+    }
+
+    @Override
+    public void expireTheURL(String key, ShortenRequest shortenRequest) {
+        String query = "UPDATE eg_url_shortener SET validto = ? WHERE id = ?";
+        jdbcTemplate.update(query, System.currentTimeMillis(), key);
+    }
+
 
 }
