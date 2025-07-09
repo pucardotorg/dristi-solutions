d8824aaadf371e76bbc3954acfd4a9e8dcc08852
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-07-09 15:35:57 +0530

    added bail bond action category

--- common/inbox/src/main/java/org/egov/inbox/web/model/ActionCategorySearchResponse.java
+++ common/inbox/src/main/java/org/egov/inbox/web/model/ActionCategorySearchResponse.java
@@ -26,4 +26,7 @@ public class ActionCategorySearchResponse {
     @JsonProperty("registerCasesData")
     private Criteria registerCasesData;
 
+    @JsonProperty("bailBondData")
+    private Criteria bailBondData;
+
 }
