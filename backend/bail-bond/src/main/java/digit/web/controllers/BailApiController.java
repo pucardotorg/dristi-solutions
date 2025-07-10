959597cbbcd3c59d06ed869b9bf6c188be6d7974
prasanna2400 <prasannajeet.v@beehyv.com>
2025-07-10 14:07:19 +0530

    Rename controller

--- backend/bail-bond/src/main/java/digit/web/controllers/BailApiController.java
+++ backend/bail-bond/src/main/java/digit/web/controllers/BailApiController.java
@@ -9,38 +9,29 @@ import digit.web.models.BailSearchResponse;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.IOException;
-import java.util.*;
 
-    import jakarta.validation.constraints.*;
-    import jakarta.validation.Valid;
+import jakarta.validation.Valid;
     import jakarta.servlet.http.HttpServletRequest;
-        import java.util.Optional;
+
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2025-07-10T12:09:26.562015481+05:30[Asia/Kolkata]")
 @Controller
     @RequestMapping("")
-    public class V1ApiController{
+    public class BailApiController {
 
         private final ObjectMapper objectMapper;
 
         private final HttpServletRequest request;
 
         @Autowired
-        public V1ApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        public BailApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
         }
