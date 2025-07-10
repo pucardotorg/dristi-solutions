d83a6f578b8c7e91c3298a0be4d05988e685e798
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-07-10 12:11:21 +0530

    added new bail-bond service

---
+++ backend/bail-bond/src/main/java/digit/web/models/BailRequest.java
@@ -0,0 +1,38 @@
+package digit.web.models;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import digit.web.models.Bail;
+import digit.web.models.RequestInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+/**
+ * BailRequest
+ */
+@Validated
+@jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2025-07-10T12:09:26.562015481+05:30[Asia/Kolkata]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BailRequest   {
+        @JsonProperty("RequestInfo")
+
+          @Valid
+                private RequestInfo requestInfo = null;
+
+        @JsonProperty("bail")
+
+          @Valid
+                private Bail bail = null;
+
+
+}
---
+++ backend/bail-bond/src/main/java/digit/web/models/BailSearchResponse.java
@@ -0,0 +1,47 @@
+package digit.web.models;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import digit.web.models.Bail;
+import digit.web.models.ResponseInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+/**
+ * BailSearchResponse
+ */
+@Validated
+@jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2025-07-10T12:09:26.562015481+05:30[Asia/Kolkata]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BailSearchResponse   {
+        @JsonProperty("ResponseInfo")
+
+          @Valid
+                private ResponseInfo responseInfo = null;
+
+        @JsonProperty("bails")
+          @Valid
+                private List<Bail> bails = null;
+
+
+        public BailSearchResponse addBailsItem(Bail bailsItem) {
+            if (this.bails == null) {
+            this.bails = new ArrayList<>();
+            }
+        this.bails.add(bailsItem);
+        return this;
+        }
+
+}
