6817f4b1ee40c462f06d6c6213d20f7b70d95fed
utpalBeehyv <utpal.kant@beehyv.com>
2025-07-20 13:40:52 +0530

    Add Validation for Save Draft (#3905)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/submissions/src/components/BailBondEsignLockModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/submissions/src/components/BailBondEsignLockModal.js
@@ -18,44 +18,53 @@ function BailBondEsignLockModal({ t, handleSaveOnSubmit, userType, filingNumber,
   };
 
   return (
-    <Modal
-      actionSaveLabel={t("CS_CLOSE")}
-      actionSaveOnSubmit={handleSaveOnSubmit}
-      className={"orders-add-bulk-list-modal"}
-      cancelButtonBody={<FileDownloadIcon></FileDownloadIcon>}
-      popupStyles={{ width: "700px", borderRadius: "4px" }}
-      style={{ width: "100%" }}
-    >
-      <div style={{ padding: "8px 0" }}>
-        <div>
-          <Banner
-            successful={true}
-            message={t(orderModalInfo?.header)}
-            headerStyles={{ fontSize: "32px" }}
-            style={{ minWidth: "100%" }}
-            svg={<BlackTickIcon color={"white"} />}
-          ></Banner>
+    <React.Fragment>
+      <style>
+        {`
+          .bailbondEsign .popup-module-main{
+          padding: 40px !important;
+          }
+          `}
+      </style>
+      <Modal
+        actionSaveLabel={t("CS_CLOSE")}
+        actionSaveOnSubmit={handleSaveOnSubmit}
+        className={"submission-success-modal bailbondEsign"}
+        cancelButtonBody={<FileDownloadIcon></FileDownloadIcon>}
+        popupStyles={{ width: "700px", borderRadius: "4px" }}
+        style={{ width: "100%" }}
+      >
+        <div style={{ padding: "8px 0" }}>
+          <div>
+            <Banner
+              whichSvg={"tick"}
+              successful={true}
+              message={t(orderModalInfo?.header)}
+              headerStyles={{ fontSize: "32px" }}
+              style={{ minWidth: "100%" }}
+            ></Banner>
 
-          {orderModalInfo?.subHeader && (
-            <CardLabel style={{ fontSize: "16px", fontWeight: 400, marginBottom: "10px", textAlign: "center" }}>
-              {t(orderModalInfo?.subHeader)}
-            </CardLabel>
-          )}
+            {orderModalInfo?.subHeader && (
+              <CardLabel style={{ fontSize: "16px", fontWeight: 400, marginBottom: "10px", textAlign: "center" }}>
+                {t(orderModalInfo?.subHeader)}
+              </CardLabel>
+            )}
 
-          <CustomCopyTextDiv
-            t={t}
-            keyStyle={{ margin: "8px 0px" }}
-            valueStyle={{ margin: "8px 0px", fontWeight: 700 }}
-            data={orderModalInfo?.url}
-            cardStyle={{ marginBottom: "16px" }}
-            subCardStyle={{ marginBottom: 0, justifyContent: "center" }}
-            isCenter={true}
-            isShowValue={false}
-            customTextStyle={{ color: "#007E7E" }}
-          />
+            <CustomCopyTextDiv
+              t={t}
+              keyStyle={{ margin: "8px 0px" }}
+              valueStyle={{ margin: "8px 0px", fontWeight: 700 }}
+              data={orderModalInfo?.url}
+              cardStyle={{ marginBottom: "16px" }}
+              subCardStyle={{ marginBottom: 0, justifyContent: "center" }}
+              isCenter={true}
+              isShowValue={false}
+              customTextStyle={{ color: "#007E7E" }}
+            />
+          </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </React.Fragment>
   );
 }
 
