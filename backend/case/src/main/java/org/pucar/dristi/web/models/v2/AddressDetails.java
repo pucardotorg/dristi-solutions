303fd109c3c61b9920e0afbe7762a46f39c7c48b
radheshjoshi1 <167431111+radheshjoshi1@users.noreply.github.com>
2025-08-04 23:17:13 +0530

    Feature 4409 witness api case (#4017)
    * add witness to the case
    
    * enhance witness details for other properties
    
    * update api spec and address code rabbit ai comments
    
    * address code rabbit ai comments
    
    * extend witness details for id and tag
    
    * extend witness api for multiple witnesses
    
    * update witness api for unique id
    
    * update witness api to support update witness
    
    * add support file
    
    * null check for mobile numbers
    
    * add validation for mobile numbers in request body

---
+++ backend/case/src/main/java/org/pucar/dristi/web/models/v2/AddressDetails.java
@@ -0,0 +1,35 @@
+package org.pucar.dristi.web.models.v2;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AddressDetails {
+
+    @JsonProperty("city")
+    private String city;
+
+    @JsonProperty("state")
+    private String state;
+
+    @JsonProperty("pincode")
+    private String pincode;
+
+    @JsonProperty("district")
+    private String district;
+
+    @JsonProperty("locality")
+    private String locality;
+
+    @JsonProperty("coordinates")
+    private Coordinates coordinates;
+
+    @JsonProperty("typeOfAddress")
+    private AddressType typeOfAddress;
+}
