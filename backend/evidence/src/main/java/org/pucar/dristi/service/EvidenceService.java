e81fbb029ec52fd34e1b54f2c650700bfbe75a83
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-08-08 18:58:08 +0530

    fix jdbctemplate query

--- backend/evidence/src/main/java/org/pucar/dristi/service/EvidenceService.java
+++ backend/evidence/src/main/java/org/pucar/dristi/service/EvidenceService.java
@@ -446,10 +446,13 @@ public class EvidenceService {
                     }
                 }
             }
+            String pseudoTag = null;
             if(WITNESS_DEPOSITION.equalsIgnoreCase(evidenceRequest.getArtifact().getArtifactType())) {
                 if(SUBMIT.equalsIgnoreCase(evidenceRequest.getArtifact().getWorkflow().getAction()) &&
                         !hasNumberSuffix(evidenceRequest.getArtifact().getTag())) {
                         evidenceEnrichment.enrichTag(evidenceRequest);
+                } else if(!hasNumberSuffix(evidenceRequest.getArtifact().getTag())) {
+                    pseudoTag = evidenceEnrichment.enrichPseudoTag(evidenceRequest);
                 }
                 updateCaseWitnessDeposition(evidenceRequest);
             }
@@ -466,6 +469,9 @@ public class EvidenceService {
                 producer.push(config.getUpdateEvidenceKafkaTopic(), evidenceRequest);
             } else {
                 producer.push(config.getUpdateEvidenceWithoutWorkflowKafkaTopic(), evidenceRequest);
+            }
+            if(pseudoTag != null) {
+                evidenceRequest.getArtifact().setTag(pseudoTag);
             }
             callNotificationService(evidenceRequest,isEvidence,false);
             return evidenceRequest.getArtifact();
