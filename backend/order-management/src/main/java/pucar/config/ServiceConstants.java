36929071a28ae3d9d8d5c9c27a1993a37195d1cb
radheshjoshi1 <167431111+radheshjoshi1@users.noreply.github.com>
2025-08-12 16:55:23 +0530

    4409 order dev (#4156)
    * check if order for witness is court or not
    
    * enhance if statement for old witnesses
    
    * add condition for pending task

--- backend/order-management/src/main/java/pucar/config/ServiceConstants.java
+++ backend/order-management/src/main/java/pucar/config/ServiceConstants.java
@@ -148,4 +148,5 @@ public class ServiceConstants {
     public static final String APPROVAL_REJECTION_ADD_WITNESS = "APPROVAL_REJECTION_ADD_WITNESS";
     public static final String ERROR_ADDING_WITNESS="ERROR_ADDING_WITNESS";
     public static final String ADDING_WITNESSES = "ADDING_WITNESSES";
+    public static final String WITNESS = "witness";
 }
