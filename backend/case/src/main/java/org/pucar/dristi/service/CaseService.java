50191d861a52391238eb5393e3a3db2ba151d04f
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-08-18 16:56:59 +0530

    file store (#4200)

--- backend/case/src/main/java/org/pucar/dristi/service/CaseService.java
+++ backend/case/src/main/java/org/pucar/dristi/service/CaseService.java
@@ -404,7 +404,7 @@ public class CaseService {
             }
 
             //todo: enhance for files delete
-//            List<Document> documentToDelete  = extractDocumentsToDelete(caseRequest.getCases(), existingApplications.get(0).getResponseList().get(0));
+            List<Document> documentToDelete  = extractDocumentsToDelete(caseRequest.getCases(), existingApplications.get(0).getResponseList().get(0));
             // Enrich application upon update
             enrichmentUtil.enrichCaseApplicationUponUpdate(caseRequest, existingApplications.get(0).getResponseList());
 
@@ -467,7 +467,7 @@ public class CaseService {
                 producer.push(config.getCaseReferenceUpdateTopic(), createHearingUpdateRequest(caseRequest));
             }
             //todo: enhance for files delete
-//            removeInactiveDocuments(documentToDelete);
+            removeInactiveDocuments(documentToDelete);
             log.info("Encrypting case: {}", caseRequest.getCases().getId());
 
             //to prevent from double encryption
