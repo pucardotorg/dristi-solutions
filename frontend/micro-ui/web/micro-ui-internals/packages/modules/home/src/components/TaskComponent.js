94766beb914b37189eb92b54638e18bb589e88f5
utpal.kant <utpal.kant@beehyv.com>
2025-09-05 17:34:36 +0530

    Filter out Litigant Review Pending Task

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/components/TaskComponent.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/components/TaskComponent.js
@@ -411,11 +411,15 @@ const TasksComponent = ({
     });
 
     const filteredTasks = tasks.filter((task) => {
-      if (isCourtRoomManager) {
-        // TODO: For court room manager,show only summons pending task, have to confirm which are those and include here.
+      // if (isCourtRoomManager) {
+      //   // TODO: For court room manager,show only summons pending task, have to confirm which are those and include here.
 
-        return task?.entityType === "bail bond" ? true : false;
-      } else return true;
+      //   return task?.entityType === "bail bond" ? true : false;
+      // } else return true;
+
+      const passesRoleFilter = isCourtRoomManager ? task?.entityType === "bail bond" : true;
+      const excludeForComposite = isApplicationCompositeOrder ? task?.actionName !== "Review Litigant Details Change" : true;
+      return passesRoleFilter && excludeForComposite;
     });
     if (taskType?.code)
       return filteredTasks?.filter((task) => taskType?.keyword?.some((key) => task?.actionName?.toLowerCase()?.includes(key?.toLowerCase())));
@@ -433,6 +437,7 @@ const TasksComponent = ({
     taskTypeCode,
     todayDate,
     userType,
+    isApplicationCompositeOrder,
   ]);
 
   const submitResponse = useCallback(
