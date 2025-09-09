3ca38fab03463ec96c0c0e3c74d1a609fb7f1f06
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-09-09 11:30:26 +0530

    moved order redesign changes from qa to dev

--- backend/order/src/main/java/org/pucar/dristi/service/OrderRegistrationService.java
+++ backend/order/src/main/java/org/pucar/dristi/service/OrderRegistrationService.java
@@ -116,6 +116,7 @@ public class OrderRegistrationService {
             // Enrich application upon update
             enrichmentUtil.enrichOrderRegistrationUponUpdate(body);
             enrichmentUtil.enrichCompositeOrderItemIdOnAddItem(body);
+            enrichmentUtil.enrichItemTextForIntermediateOrder(body);
 
             workflowUpdate(body);
 
