83e96d33be4afcd46046d52ad1e7c89c9a578869
Prasannajeet Meshram <107095851+prasanna2400@users.noreply.github.com>
2025-09-19 14:39:11 +0530

    Fix error in index creation migration file (#4535)

--- backend/task/src/main/resources/db/migration/main/V20250918130230__task__ddl.sql
+++ backend/task/src/main/resources/db/migration/main/V20250918130230__task__ddl.sql
@@ -6,7 +6,7 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_task_notice_type
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_task_delivery_channel_obj
     ON dristi_task ((taskdetails -> 'deliveryChannels' ->> 'channelName'));
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_task_delivery_channel_elem
-    ON dristi_task USING GIN ((taskdetails -> 'deliveryChannels')) jsonb_path_ops;
+    ON dristi_task USING GIN ((taskdetails -> 'deliveryChannels'));
 
 -- hearingDate epoch (as text cast to bigint)
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_task_hearing_date_epoch
