d253613bf04f449fd8cfd624ef5269d55501b10e
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-09-26 15:34:27 +0530

    added condition to filter

--- backend/order/src/main/java/org/pucar/dristi/repository/querybuilder/OrderQueryBuilder.java
+++ backend/order/src/main/java/org/pucar/dristi/repository/querybuilder/OrderQueryBuilder.java
@@ -113,7 +113,9 @@ public class OrderQueryBuilder {
             firstCriteria = addCriteria(criteria.getHearingNumber(), query, firstCriteria, "orders.hearingNumber = ?", preparedStmtList, preparedStmtArgList, Types.VARCHAR);
             firstCriteria = addCriteria(criteria.getScheduledHearingNumber(), query, firstCriteria, "orders.scheduledhearingnumber", preparedStmtList, preparedStmtArgList, Types.VARCHAR);
             firstCriteria = addCriteria(criteria.getCreatedTime() != null ? criteria.getCreatedTime().toString() : null, query, firstCriteria, "orders.createdTime >= ?", preparedStmtList, preparedStmtArgList, Types.VARCHAR);
-            firstCriteria = addCriteria(String.valueOf(System.currentTimeMillis()), query, firstCriteria, "orders.createdTime <= ?", preparedStmtList, preparedStmtArgList, Types.VARCHAR);
+            if (criteria.getCreatedTime() != null) {
+                firstCriteria = addCriteria(String.valueOf(System.currentTimeMillis()), query, firstCriteria, "orders.createdTime <= ?", preparedStmtList, preparedStmtArgList, Types.VARCHAR);
+            }
 
             if (criteria.getIsFuzzySearch() == null || !criteria.getIsFuzzySearch()) {
                 addCriteria(criteria.getOrderNumber() , query, firstCriteria, "LOWER(orders.orderNumber) = LOWER(?)", preparedStmtList, preparedStmtArgList, Types.VARCHAR);
