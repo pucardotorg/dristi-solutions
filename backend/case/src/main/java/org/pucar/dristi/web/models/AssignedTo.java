ca7f7931ace80a5085860093fa6fe18affbb8006
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-10-01 14:05:54 +0530

    assignedto mapping error in task create (#4633)
    * assignedto mapping error in task create
    
    * added constructor

--- backend/case/src/main/java/org/pucar/dristi/web/models/AssignedTo.java
+++ backend/case/src/main/java/org/pucar/dristi/web/models/AssignedTo.java
@@ -2,7 +2,9 @@ package org.pucar.dristi.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
@@ -13,6 +15,8 @@ import java.util.UUID;
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-18T11:14:11.072458+05:30[Asia/Calcutta]")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssignedTo {
 
     @JsonProperty("individualId")
