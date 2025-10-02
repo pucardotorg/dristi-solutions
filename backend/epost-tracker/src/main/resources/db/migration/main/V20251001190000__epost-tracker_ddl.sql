f93f9558d691b66ffe1875c1c20386d6e2609182
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-10-02 12:24:37 +0530

    db migration changes

--- backend/epost-tracker/src/main/resources/db/migration/main/V20251001190000__epost-tracker_ddl.sql
+++ backend/epost-tracker/src/main/resources/db/migration/main/V20251001190000__epost-tracker_ddl.sql
@@ -1,5 +1,17 @@
 ALTER TABLE dristi_epost_tracker
 ALTER COLUMN booking_date TYPE int8
-USING (EXTRACT(EPOCH FROM (to_timestamp(booking_date, 'YYYY-MM-DD') AT TIME ZONE 'Asia/Kolkata')) * 1000)::bigint,
+USING (
+  CASE
+    WHEN booking_date IS NULL OR booking_date = '' OR booking_date = 'null' THEN NULL
+    ELSE (EXTRACT(EPOCH FROM (to_timestamp(booking_date, 'YYYY-MM-DD') AT TIME ZONE 'Asia/Kolkata')) * 1000)::bigint
+  END
+);
+
+ALTER TABLE dristi_epost_tracker
 ALTER COLUMN received_date TYPE int8
-USING (EXTRACT(EPOCH FROM (to_timestamp(received_date, 'YYYY-MM-DD') AT TIME ZONE 'Asia/Kolkata')) * 1000)::bigint;
+USING (
+  CASE
+    WHEN received_date IS NULL OR received_date = '' OR received_date = 'null' THEN NULL
+    ELSE (EXTRACT(EPOCH FROM (to_timestamp(received_date, 'YYYY-MM-DD') AT TIME ZONE 'Asia/Kolkata')) * 1000)::bigint
+  END
+);
