67600b38623e46080f849c130279befc90124fd9
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-10-03 11:17:30 +0530

    dropping the columns and altering the data

--- backend/epost-tracker/src/main/resources/db/migration/main/V20251001190000__epost-tracker_ddl.sql
+++ backend/epost-tracker/src/main/resources/db/migration/main/V20251001190000__epost-tracker_ddl.sql
@@ -1,17 +1,7 @@
 ALTER TABLE dristi_epost_tracker
-ALTER COLUMN booking_date TYPE int8
-USING (
-  CASE
-    WHEN booking_date IS NULL OR booking_date = '' OR booking_date = 'null' THEN NULL
-    ELSE (EXTRACT(EPOCH FROM (to_timestamp(booking_date, 'YYYY-MM-DD') AT TIME ZONE 'Asia/Kolkata')) * 1000)::bigint
-  END
-);
+DROP COLUMN IF EXISTS booking_date,
+DROP COLUMN IF EXISTS received_date;
 
-ALTER TABLE dristi_epost_tracker
-ALTER COLUMN received_date TYPE int8
-USING (
-  CASE
-    WHEN received_date IS NULL OR received_date = '' OR received_date = 'null' THEN NULL
-    ELSE (EXTRACT(EPOCH FROM (to_timestamp(received_date, 'YYYY-MM-DD') AT TIME ZONE 'Asia/Kolkata')) * 1000)::bigint
-  END
-);
+ALTER TABLE dristi_epost_tracker 
+ADD COLUMN booking_date int8,
+ADD COLUMN received_date int8;
