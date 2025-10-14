0cd2b18b90940785998f90977bd52b4de74d24e0
assim-beehyv <assim.ujjwal@beehyv.com>
2025-10-14 20:07:56 +0530

    update hearingSummary on publish order only. (#4782)
    * update hearingSummary on publish order only.
    
    * Bulk sign: update hearingSummary on publish order only.
    
    * Minor change.
    
    ---------
    
    Co-authored-by: Assim Ujjwal <assimfrommohania@gmail.com>

--- backend/order-management/src/main/java/pucar/service/BSSService.java
+++ backend/order-management/src/main/java/pucar/service/BSSService.java
@@ -2,6 +2,7 @@ package pucar.service;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.egov.common.contract.request.RequestInfo;
 import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -14,6 +15,9 @@ import pucar.util.*;
 import pucar.web.models.*;
 import pucar.web.models.adiary.BulkDiaryEntryRequest;
 import pucar.web.models.adiary.CaseDiaryEntry;
+import pucar.web.models.hearing.Hearing;
+import pucar.web.models.hearing.HearingCriteria;
+import pucar.web.models.hearing.HearingSearchRequest;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -216,6 +220,7 @@ public class BSSService {
                     if (order.getNextHearingDate() != null) {
                         hearingUtil.preProcessScheduleNextHearing(orderUpdateRequest);
                     }
+                    updateHearingSummary(orderUpdateRequest);
                     OrderResponse response = orderUtil.updateOrder(orderUpdateRequest);
                     List<CaseDiaryEntry> diaryEntries = orderProcessor.processCommonItems(orderUpdateRequest);
                     caseDiaryEntries.addAll(diaryEntries);
@@ -240,6 +245,21 @@ public class BSSService {
 
         return updatedOrder;
 
+    }
+
+    private void updateHearingSummary(OrderRequest request) {
+
+        Order order = request.getOrder();
+        RequestInfo requestInfo = request.getRequestInfo();
+
+        //If attendance is present then attendance and item text will go in hearing summary
+        if (order.getAttendance() != null) {
+            String hearingNumber = hearingUtil.getHearingNumberFormApplicationAdditionalDetails(order.getAdditionalDetails());
+            List<Hearing> hearings = hearingUtil.fetchHearing(HearingSearchRequest.builder().requestInfo(requestInfo)
+                    .criteria(HearingCriteria.builder().hearingId(hearingNumber).tenantId(order.getTenantId()).build()).build());
+            Hearing hearing = hearings.get(0);
+            hearingUtil.updateHearingSummary(request, hearing);
+        }
     }
 
     private Map<String, Object> createAttribute(String name, String value) {
