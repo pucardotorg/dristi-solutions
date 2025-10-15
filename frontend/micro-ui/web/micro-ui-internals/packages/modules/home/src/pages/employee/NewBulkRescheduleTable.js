4b89e51ad0297270a45aaa3e780aae0b179731b2
ashishbeehyv <ashish.y@beehyv.com>
2025-10-15 12:28:21 +0530

    localizatiojn

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/NewBulkRescheduleTable.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/NewBulkRescheduleTable.js
@@ -130,7 +130,7 @@ const NewBulkRescheduleTable = ({
           <div className={`case-label-field-pair search-input`}>
             <input
               className="home-input"
-              placeholder="Search Case name or number"
+              placeholder={t("SEARCH_CASE_NAME_OR_NUMBER")} 
               style={{ width: "280px" }}
               type="text"
               value={bulkFormData?.searchableFields || ""}
