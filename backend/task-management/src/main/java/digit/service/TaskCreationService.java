3b51f32af4ae589776162bb63722a1781771747f
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-11-07 19:38:22 +0530

    added support folder and dev testing fixes

--- backend/task-management/src/main/java/digit/service/TaskCreationService.java
+++ backend/task-management/src/main/java/digit/service/TaskCreationService.java
@@ -351,7 +351,7 @@ public class TaskCreationService {
         ObjectNode compositeItem = extractCompositeItems(order, itemId);
 
         if (compositeItem != null) {
-            return jsonUtil.getNestedValue(compositeItem, List.of("orderSchema", "additionalDetails","formdata", "noticeType", "type"), String.class);
+            compositeItem.path("orderSchema").path("additionalDetails").path("formdata").path("noticeType").path("type").textValue();
         }
 
         return null;
@@ -362,7 +362,7 @@ public class TaskCreationService {
         ObjectNode compositeItem = extractCompositeItems(order, itemId);
 
         if (compositeItem != null) {
-            return jsonUtil.getNestedValue(compositeItem, List.of("orderSchema", "additionalDetails","formdata", "dateForHearing"), String.class);
+            compositeItem.path("orderSchema").path("additionalDetails").path("formdata").path("dateForHearing").textValue();
         }
 
         return null;
