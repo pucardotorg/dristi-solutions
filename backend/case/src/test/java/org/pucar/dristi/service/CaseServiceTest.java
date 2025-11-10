6fe57be850c1943ecb42235634c0b503af20e889
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-11-10 15:45:05 +0530

    livecaulsist changes from dev

--- backend/case/src/test/java/org/pucar/dristi/service/CaseServiceTest.java
+++ backend/case/src/test/java/org/pucar/dristi/service/CaseServiceTest.java
@@ -117,6 +117,9 @@ public class CaseServiceTest {
     @Mock
     private DateUtil dateUtil;
 
+    @Mock
+    private InboxUtil inboxUtil;
+
     private CaseRequest caseRequest;
     private RequestInfo requestInfo;
     private User userInfo;
@@ -179,7 +182,7 @@ public class CaseServiceTest {
         objectMapper = new ObjectMapper();
         enrichmentService = new EnrichmentService(new ArrayList<>());
         OrderUtil orderUtil = new OrderUtil(null, null, null);
-        caseService = new CaseService(validator,enrichmentUtil,caseRepository,workflowService,config,producer,taskUtil,etreasuryUtil,encryptionDecryptionUtil, hearingUtil,userService,paymentCalculaterUtil,objectMapper,cacheService,enrichmentService, notificationService, individualService, advocateUtil, evidenceUtil, evidenceValidator,caseUtil,fileStoreUtil, orderUtil, dateUtil);
+        caseService = new CaseService(validator,enrichmentUtil,caseRepository,workflowService,config,producer,taskUtil,etreasuryUtil,encryptionDecryptionUtil, hearingUtil,userService,paymentCalculaterUtil,objectMapper,cacheService,enrichmentService, notificationService, individualService, advocateUtil, evidenceUtil, evidenceValidator,caseUtil,fileStoreUtil, orderUtil, dateUtil,inboxUtil );
 
         requestInfo = RequestInfo.builder()
                 .userInfo(User.builder().uuid("ba8767a6-7cb1-416b-803e-19cf9dca06bc").tenantId(TENANT_ID).build())
