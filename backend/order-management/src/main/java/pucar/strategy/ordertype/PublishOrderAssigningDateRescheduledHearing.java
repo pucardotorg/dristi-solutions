97567456d3797252612cc357321b4c4aa15bef45
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-11-14 20:48:47 +0530

    reschedule hearing failing

--- backend/order-management/src/main/java/pucar/strategy/ordertype/PublishOrderAssigningDateRescheduledHearing.java
+++ backend/order-management/src/main/java/pucar/strategy/ordertype/PublishOrderAssigningDateRescheduledHearing.java
@@ -91,9 +91,16 @@ public class PublishOrderAssigningDateRescheduledHearing implements OrderUpdateS
             hearingNumber = orderUtil.getHearingNumberFormApplicationAdditionalDetails(applications.get(0).getAdditionalDetails());
         }
         log.info("hearingNumber:{}", hearingNumber);
-        List<Hearing> hearings = hearingUtil.fetchHearing(HearingSearchRequest.builder().requestInfo(requestInfo)
-                .criteria(HearingCriteria.builder().hearingId(hearingNumber).tenantId(order.getTenantId()).build()).build());
-        Hearing hearing = hearings.get(0);
+        Hearing hearing = null;
+        if(hearingNumber!=null) {
+            List<Hearing> hearings = hearingUtil.fetchHearing(HearingSearchRequest.builder().requestInfo(requestInfo)
+                    .criteria(HearingCriteria.builder().hearingId(hearingNumber).tenantId(order.getTenantId()).build()).build());
+             hearing = hearings.get(0);
+        }else{
+            List<Hearing> hearings = hearingUtil.fetchHearing(HearingSearchRequest.builder().requestInfo(requestInfo)
+                    .criteria(HearingCriteria.builder().hearingId(order.getScheduledHearingNumber()).tenantId(order.getTenantId()).build()).build());
+            hearing = hearings.get(0);
+        }
 
         order.setHearingNumber(hearing.getHearingId());
 
