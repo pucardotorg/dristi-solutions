6b710b3e2fc48fbd4a6294ca282d706b024b70bf
TusharKatiyar2002 <tushar.k@beehyv.com>
2025-11-19 18:57:44 +0530

    added uniqueId in taskDetails of accused and witness in warrant

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pages/employee/GenerateOrdersV2.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pages/employee/GenerateOrdersV2.js
@@ -2430,11 +2430,12 @@ const GenerateOrdersV2 = () => {
 
     const respondentDetails = {
       name: respondentName,
-      address: respondentAddress?.[0],
+      address: { ...respondentAddress?.[0], coordinate: respondentAddress?.[0]?.coordinates },
       phone: respondentPhoneNo[0] || "",
       email: respondentEmail[0] || "",
       age: "",
       gender: "",
+      uniqueId: respondentUniqueId,
       ...(ownerType && { ownerType: ownerType }),
     };
     const caseRespondent = {
@@ -2527,16 +2528,8 @@ const GenerateOrdersV2 = () => {
             templateType: orderFormValue?.warrantSubType?.templateType || "GENERIC",
             warrantText: orderFormValue?.warrantText?.warrantText || "",
           },
-          respondentDetails: {
-            name: respondentName,
-            address: { ...respondentAddress?.[0], coordinate: respondentAddress?.[0]?.coordinates },
-            phone: respondentPhoneNo?.[0] || "",
-            email: respondentEmail?.[0] || "",
-            age: "",
-            gender: "",
-            uniqueId: respondentUniqueId,
-            ...(ownerType && { ownerType: ownerType }),
-          },
+          ...(orderFormData?.party?.data?.partyType === "Witness" && { witnessDetails: respondentDetails }),
+          respondentDetails: respondentDetails,
           caseDetails: {
             caseTitle: caseDetails?.caseTitle,
             year: new Date(caseDetails).getFullYear(),
@@ -2575,16 +2568,8 @@ const GenerateOrdersV2 = () => {
             proclamationText: orderFormValue?.proclamationText?.proclamationText || "",
             partyType: respondentNameData?.partyType?.toLowerCase() || "accused",
           },
-          respondentDetails: {
-            name: respondentName,
-            address: { ...respondentAddress?.[0], coordinate: respondentAddress?.[0]?.coordinates },
-            phone: respondentPhoneNo?.[0] || "",
-            email: respondentEmail?.[0] || "",
-            age: "",
-            gender: "",
-            uniqueId: respondentUniqueId,
-            ...(ownerType && { ownerType: ownerType }),
-          },
+          ...(orderFormData?.party?.data?.partyType === "Witness" && { witnessDetails: respondentDetails }),
+          respondentDetails: respondentDetails,
           caseDetails: {
             caseTitle: caseDetails?.caseTitle,
             year: new Date(caseDetails).getFullYear(),
@@ -2626,16 +2611,8 @@ const GenerateOrdersV2 = () => {
             chargeDays: orderFormValue?.chargeDays?.chargeDays || "",
             partyType: respondentNameData?.partyType?.toLowerCase() || "accused",
           },
-          respondentDetails: {
-            name: respondentName,
-            address: { ...respondentAddress?.[0], coordinate: respondentAddress?.[0]?.coordinates },
-            phone: respondentPhoneNo?.[0] || "",
-            email: respondentEmail?.[0] || "",
-            age: "",
-            gender: "",
-            uniqueId: respondentUniqueId,
-            ...(ownerType && { ownerType: ownerType }),
-          },
+          ...(orderFormData?.party?.data?.partyType === "Witness" && { witnessDetails: respondentDetails }),
+          respondentDetails: respondentDetails,
           caseDetails: {
             caseTitle: caseDetails?.caseTitle,
             year: new Date(caseDetails).getFullYear(),
@@ -3527,7 +3504,7 @@ const GenerateOrdersV2 = () => {
     sessionStorage.removeItem("fileStoreId");
     sessionStorage.removeItem("businessOfTheDay");
     setSignedDocumentUploadID("");
-    setFileStoreIds(prev => {
+    setFileStoreIds((prev) => {
       const updated = new Set(prev);
       updated.delete(signedDoucumentUploadedID);
       return updated;
