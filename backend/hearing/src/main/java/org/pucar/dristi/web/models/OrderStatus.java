d4d5dfcb3f8bee3c6d8cc34e40480bd936d833d8
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-11-21 17:54:47 +0530

    added orderStatus in open hearing index

---
+++ backend/hearing/src/main/java/org/pucar/dristi/web/models/OrderStatus.java
@@ -0,0 +1,22 @@
+package org.pucar.dristi.web.models;
+
+public enum OrderStatus {
+
+    PENDING_SIGN("pending_sign"),
+    SIGNED("signed");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
