75c959e08e613381788d78b124171317cb167448
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-11-25 12:55:50 +0530

    create new draft order if previous delted

--- backend/order-management/src/main/java/pucar/web/models/inbox/OrderBy.java
+++ backend/order-management/src/main/java/pucar/web/models/inbox/OrderBy.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.pucar.dristi.web.models.Order;
 
 @Data
 @AllArgsConstructor
