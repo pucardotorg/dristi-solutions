71ae2cf9585a204728ac270bd7b6132958f4baab
rilsarojes26 <rilsarojes26@gmail.com>
2025-11-25 15:48:24 +0530

    Missing Data Validations on Old eFiling Screen

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/configs/ordersCreateConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/configs/ordersCreateConfig.js
@@ -5750,6 +5750,9 @@ export const configsCost = [
           error: "CORE_REQUIRED_FIELD_ERROR",
           componentInFront: "₹",
           prefix: "",
+          validation: {
+            pattern: "^[0-9]+(\\.[0-9]{0,2})?$",
+          },
         },
       },
       {
