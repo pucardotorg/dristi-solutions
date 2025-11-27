2f8c4a5b790aca388f3d15e75b88f3a658a2d594
Assim Ujjwal <assimfrommohania@gmail.com>
2025-11-27 16:29:56 +0530

    settlement order config

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/configs/ordersCreateConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/configs/ordersCreateConfig.js
@@ -2888,6 +2888,27 @@ export const configsCaseSettlement = [
           ],
         },
       },
+      {
+        label: "NATURE_OF_DISPOSAL",
+        isMandatory: true,
+        key: "natureOfDisposal",
+        schemaKeyPath: "orderDetails.natureOfDisposal",
+        transformer: "mdmsDropdown",
+        type: "dropdown",
+        populators: {
+          name: "natureOfDisposal",
+          optionsKey: "name",
+          error: "CORE_REQUIRED_FIELD_ERROR",
+          styles: { maxWidth: "100%" },
+          required: true,
+          isMandatory: true,
+          mdmsConfig: {
+            moduleName: "Order",
+            masterName: "natureOfDisposal",
+            select: "(data) => {return data['Order'].natureOfDisposal?.map((item) => {return item;});}",
+          },
+        },
+      },
     ],
   },
   {
