d13326179cc4310c838c17ba776d55515d85d28b
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2025-11-27 21:20:12 +0530

    added tracer and bug fix (#5295)
    (cherry picked from commit 27fdcca7a16a4dee7d311e138449cd8c2fecdd3f)

--- backend/transformer/src/main/java/org/egov/transformer/TransformerApplication.java
+++ backend/transformer/src/main/java/org/egov/transformer/TransformerApplication.java
@@ -1,11 +1,11 @@
 package org.egov.transformer;
 
-import org.egov.transformer.config.MainConfiguration;
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import({MainConfiguration.class})
+@Import({TracerConfiguration.class})
 @SpringBootApplication
 public class TransformerApplication {
     public static void main(String[] args) {
