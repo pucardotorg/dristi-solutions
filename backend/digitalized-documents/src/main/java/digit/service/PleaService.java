51a6286859b6a86aaaea5527c631e8c6e4a2b36f
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2025-11-28 16:15:17 +0530

    plea implementation

---
+++ backend/digitalized-documents/src/main/java/digit/service/PleaService.java
@@ -0,0 +1,62 @@
+package digit.service;
+
+import digit.config.Configuration;
+import digit.enrichment.DigitalizedDocumentEnrichment;
+import digit.enrichment.PleaEnrichment;
+import digit.kafka.Producer;
+import digit.validators.PleaValidator;
+import digit.web.models.DigitalizedDocument;
+import digit.web.models.DigitalizedDocumentRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for processing PLEA type documents
+ */
+@Service
+@Slf4j
+public class PleaService implements DocumentTypeService {
+
+    private final PleaValidator validator;
+    private final DigitalizedDocumentEnrichment enrichment;
+    private final PleaEnrichment pleaEnrichment;
+    private final WorkflowService workflowService;
+    private final Producer producer;
+    private final Configuration config;
+
+    public PleaService(PleaValidator validator, DigitalizedDocumentEnrichment enrichment, PleaEnrichment pleaEnrichment, WorkflowService workflowService, Producer producer, Configuration config) {
+        this.validator = validator;
+        this.enrichment = enrichment;
+        this.pleaEnrichment = pleaEnrichment;
+        this.workflowService = workflowService;
+        this.producer = producer;
+        this.config = config;
+    }
+
+    @Override
+    public DigitalizedDocument createDocument(DigitalizedDocumentRequest request) {
+
+        enrichment.enrichDigitalizedDocument(request);
+
+        pleaEnrichment.enrichDocumentOnCreation(request);
+
+        workflowService.updateWorkflowStatus(request);
+
+        producer.push(config.getPleaDigitalizedDocumentCreateTopic(), request);
+
+        return request.getDigitalizedDocument();
+    }
+
+    @Override
+    public DigitalizedDocument updateDocument(DigitalizedDocumentRequest request) {
+        validator.validateDigitalizedDocument(request.getDigitalizedDocument());
+
+        pleaEnrichment.enrichDocumentOnUpdate(request);
+
+        workflowService.updateWorkflowStatus(request);
+
+        producer.push(config.getPleaDigitalizedDocumentUpdateTopic(), request);
+
+        return request.getDigitalizedDocument();
+    }
+}
