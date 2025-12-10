a5a5a55dfa3558311dbf531111e36ab63d8f8f83
Assim Ujjwal <assimfrommohania@gmail.com>
2025-12-10 13:52:41 +0530

    Revert "bug in case efiling when jumped through side panel (#5383)"
    This reverts commit df0cccf06a69eedb1e61d03baf24340d8e120154.

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/EfilingValidationUtils.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/EfilingValidationUtils.js
@@ -1726,7 +1726,7 @@ export const updateCaseDetails = async ({
     const poaFilestoreIds = {};
     // check -in new flow, mltiple complainant forms are possible, so iscompleted logic has to be updated
     // and logic to update litigants also has to be changed.
-    // if (isCompleted === true) {
+    if (isCompleted === true) {
       litigants = await Promise.all(
         updatedFormData
           .filter((item) => item.isenabled)
@@ -2239,7 +2239,7 @@ export const updateCaseDetails = async ({
             }
           })
       );
-    // }
+    }
 
     let docList = [];
     const newFormData = await Promise.all(
