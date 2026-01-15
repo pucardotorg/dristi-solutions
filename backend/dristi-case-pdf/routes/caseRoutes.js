66ced2323a9812520983a40b7f1b7765327a0811
shashankbk <shashank.balakrishnan@beehyv.com>
2026-01-15 19:32:56 +0530

    Reorganize directories

--- backend/dristi-case-pdf/routes/caseRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const caseController = require("../controller/caseController");
-
-router.post("/v1/generateCasePdf", caseController.generateCasePdf);
-router.post("/v1/fetchCaseComplaintPdf", caseController.caseComplaintPdf);
-
-module.exports = router;
--- utilities/dristi-pdf/src/utils/extractOrderNumber.js
+++
@@ -1,8 +0,0 @@
-function extractOrderNumber(orderItemId) {
-  if (!orderItemId || typeof orderItemId !== "string") return orderItemId || "";
-  return orderItemId?.includes("_")
-    ? orderItemId?.split("_")?.pop()
-    : orderItemId;
-}
-
-module.exports = { extractOrderNumber };
