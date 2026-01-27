88fd35eef0efb261d138b54cc2e8ea39c561f3c6
TusharKatiyar2002 <tushar.k@beehyv.com>
2026-01-27 18:31:07 +0530

    allowing employees to upload at any time in mediation form

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/utils/orderApiCallUtils.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/utils/orderApiCallUtils.js
@@ -101,6 +101,7 @@ export const addOrderItem = async (
           isMediationChanged: isMediationChanged,
           dateOfEndADR: orderSchema?.orderDetails?.hearingDate,
           mediationCentre: t(orderSchema?.orderDetails?.mediationCentre) || "",
+          modeOfSigning: "INITIATE_E-SIGN",
         }),
         ...(order?.orderType === "ACCEPT_RESCHEDULING_REQUEST" && { currentDate: new Date().getTime() }),
       },
