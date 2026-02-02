e1c2eabdb6fb005a6aa8cc0f3236fa64e7f105ff
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2026-02-02 18:05:10 +0530

    update hearigType on reschedule (#5860)
    * update hearigType on reschedule
    
    * update hearigType on reschedule

--- backend/dristi-services/hearing/src/main/java/org/pucar/dristi/service/HearingService.java
+++ backend/dristi-services/hearing/src/main/java/org/pucar/dristi/service/HearingService.java
@@ -150,11 +150,12 @@ public class HearingService {
             hearing.setCmpNumber(hearingRequest.getHearing().getCmpNumber() != null ? hearingRequest.getHearing().getCmpNumber() : hearing.getCmpNumber());
             hearing.setCourtCaseNumber(hearingRequest.getHearing().getCourtCaseNumber() != null ? hearingRequest.getHearing().getCourtCaseNumber() : hearing.getCourtCaseNumber());
             hearing.setCaseReferenceNumber(hearingRequest.getHearing().getCaseReferenceNumber() != null ? hearingRequest.getHearing().getCaseReferenceNumber() : hearing.getCaseReferenceNumber());
-            hearingRequest.setHearing(hearing);
-
+            String newHearingType = null;
             if(hearing.getWorkflow()!=null && (hearing.getWorkflow().getAction().equalsIgnoreCase(MARK_COMPLETE) || hearing.getWorkflow().getAction().equalsIgnoreCase(UPDATE_DATE))){
-                hearing.setHearingType(hearingRequest.getHearing().getHearingType());
+                newHearingType = hearingRequest.getHearing().getHearingType();
+                hearing.setHearingType(newHearingType);
             }
+            hearingRequest.setHearing(hearing);
 
             // Enrich application upon update
             enrichmentUtil.enrichHearingApplicationUponUpdate(hearingRequest);
