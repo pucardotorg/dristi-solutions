bba578276de057c4bb6c8b350e6bd33333b20ff0
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2026-02-02 18:04:08 +0530

    update hearigType on reschedule

--- backend/dristi-services/hearing/src/main/java/org/pucar/dristi/service/HearingService.java
+++ backend/dristi-services/hearing/src/main/java/org/pucar/dristi/service/HearingService.java
@@ -150,8 +150,10 @@ public class HearingService {
             hearing.setCmpNumber(hearingRequest.getHearing().getCmpNumber() != null ? hearingRequest.getHearing().getCmpNumber() : hearing.getCmpNumber());
             hearing.setCourtCaseNumber(hearingRequest.getHearing().getCourtCaseNumber() != null ? hearingRequest.getHearing().getCourtCaseNumber() : hearing.getCourtCaseNumber());
             hearing.setCaseReferenceNumber(hearingRequest.getHearing().getCaseReferenceNumber() != null ? hearingRequest.getHearing().getCaseReferenceNumber() : hearing.getCaseReferenceNumber());
+            String newHearingType = null;
             if(hearing.getWorkflow()!=null && (hearing.getWorkflow().getAction().equalsIgnoreCase(MARK_COMPLETE) || hearing.getWorkflow().getAction().equalsIgnoreCase(UPDATE_DATE))){
-                hearing.setHearingType(hearingRequest.getHearing().getHearingType());
+                newHearingType = hearingRequest.getHearing().getHearingType();
+                hearing.setHearingType(newHearingType);
             }
             hearingRequest.setHearing(hearing);
 
