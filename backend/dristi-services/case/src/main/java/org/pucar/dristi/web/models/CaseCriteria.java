cbb3b7d76eeba536701a619f269afa59240f6ad3
shashankbk <shashank.balakrishnan@beehyv.com>
2026-02-04 20:59:04 +0530

    Case search enhancements

--- backend/dristi-services/case/src/main/java/org/pucar/dristi/web/models/CaseCriteria.java
+++ backend/dristi-services/case/src/main/java/org/pucar/dristi/web/models/CaseCriteria.java
@@ -98,13 +98,7 @@ public class CaseCriteria {
     @Valid
     private Pagination pagination = null;
 
-    @JsonProperty("officeAdvocateId")
-    private String officeAdvocateId = null;
-
-    @JsonProperty("memberId")
-    private String memberId = null;
-
-    @JsonProperty("isMemberActiveInCase")
-    private Boolean isMemberActiveInCase = null;
+    @JsonProperty("isClerk")
+    private Boolean isClerk = null;
 
 }
