162460190f55cc88cbba1aedb0727c3da2285981
TusharKatiyar2002 <163826002+TusharKatiyar2002@users.noreply.github.com>
2026-02-18 13:44:16 +0530

    fixed placeHolder issue in case filing esigning (#6025)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/ComplainantSignature.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/ComplainantSignature.js
@@ -661,7 +661,18 @@ const ComplainantSignature = ({ path }) => {
     } else {
       if (isOwnerAdvocateSelf) {
         const advocate = caseDetails?.representatives?.find((advocate) => advocate?.additionalDetails?.uuid === loggedInUserOnBehalfOfUuid);
-        placeholder = `Advocate ${advocate?.representing?.[0]?.additionalDetails?.currentPosition} Signature`;
+        const representingWithAllUnsigned = advocate?.representing?.find((rep) => {
+          // match litigant using UUID
+          const litigant = litigants?.find((lit) => lit?.additionalDetails?.uuid === rep?.additionalDetails?.uuid);
+
+          // if no litigant → skip
+          if (!litigant?.representatives?.length) return false;
+
+          // check all representatives unsigned
+          return litigant.representatives.every((r) => r?.hasSigned === false);
+        });
+
+        placeholder = `Advocate ${representingWithAllUnsigned?.additionalDetails?.currentPosition} Signature`;
         return placeholder; // Return placeholder directly for advocate filing case
       } else {
         const litigant = litigants?.find((litigant) => litigant?.additionalDetails?.uuid === loggedInUserOnBehalfOfUuid);
