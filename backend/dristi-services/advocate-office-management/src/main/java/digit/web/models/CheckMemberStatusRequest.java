467572761f18dc51315c4d6a99d0816cc572209f
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2026-02-23 16:29:52 +0530

    added support folder for role action mapping

--- backend/dristi-services/advocate-office-management/src/main/java/digit/web/models/CheckMemberStatusRequest.java
+++ backend/dristi-services/advocate-office-management/src/main/java/digit/web/models/CheckMemberStatusRequest.java
@@ -1,6 +1,7 @@
 package digit.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,13 +17,20 @@ public class CheckMemberStatusRequest {
 
     @JsonProperty("RequestInfo")
     @NotNull
+    @Valid
     private RequestInfo requestInfo;
 
     @JsonProperty("advocateUserUuid")
     @NotNull
+    @Valid
     private String advocateUserUuid;
 
     @JsonProperty("memberUserUuid")
     @NotNull
+    @Valid
     private String memberUserUuid;
+
+    @JsonProperty("tenantId")
+    @Valid
+    private String tenantId;
 }
