681aec938ba5673785d3f1ad33da852bfcbf6976
Nilesh Gupta <165361275+nileshgupta111@users.noreply.github.com>
2026-02-24 09:50:45 +0000

    upgrade java 17 to java 25

--- backend/dristi-services/case/src/main/java/org/pucar/dristi/kafka/Producer.java
+++ backend/dristi-services/case/src/main/java/org/pucar/dristi/kafka/Producer.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("kafkaProducer")
+@Service
 @Slf4j
 public class Producer {
 
