6b26f8c3d1ecab1f67979e07287d470a04ed3744
shashankbk-beehyv <shashank.balakrishnan@beehyv.com>
2026-02-24 11:57:02 +0530

    Add service for fetching bank details (#6051)
    * Add service for fetching bank details from IFSC code
    
    * Update config.json
    
    * Update config.json
    
    * Refactor /bank-details to /integration-services
    
    * Address code rabbit comments
    
    * Optimize imports

---
+++ backend/integration-services/bank-details/src/main/java/org/pucar/dristi/web/models/BankDetailsSearchCriteria.java
@@ -0,0 +1,29 @@
+package org.pucar.dristi.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+/**
+ * BankDetailsSearchCriteria
+ */
+@Validated
+@javax.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2026-02-19T19:24:10.916325138+05:30[Asia/Kolkata]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BankDetailsSearchCriteria {
+
+    @JsonProperty("ifsc")
+    @NotBlank(message = "IFSC code must not be blank")
+    @Pattern(regexp = "^[A-Z]{4}0[A-Z0-9]{6}$", message = "Invalid IFSC code format")
+    private String ifsc = null;
+
+}
