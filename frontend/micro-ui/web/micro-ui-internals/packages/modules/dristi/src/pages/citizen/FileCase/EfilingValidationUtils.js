06fc7e867f79df726526bac7b33a301f6d781544
Rashi <rasshi.beehyv@gmail.com>
2026-03-11 16:19:22 +0530

    On wrong ifsc double search no error was coming

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/EfilingValidationUtils.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/FileCase/EfilingValidationUtils.js
@@ -391,8 +391,6 @@ export const handleIfscAutofill = async ({ ifsc, bankField, branchField, setValu
     bankDetails = await fetchBankDetails(ifsc);
 
     if (!bankDetails) {
-      cache.current[ifsc] = "FAILED";
-      // setError(configKey, { msg: "CS_INVALID_IFSC" });
       return false;
     }
 
@@ -411,10 +409,6 @@ export const handleIfscAutofill = async ({ ifsc, bankField, branchField, setValu
     setValue(branchField, bankDetails.branch || "");
     clearErrors(branchField);
   }
-  const prefix = bankField.replace("BankName", "");
-
-  setValue(`${prefix}BankReadOnly`, true);
-  setValue(`${prefix}BranchReadOnly`, true);
 
   return true;
 };
