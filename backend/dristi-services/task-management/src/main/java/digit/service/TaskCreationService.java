1837c699fe8698a9f8305c59491b5811434529cb
Shashank Balakrishnan <shashank.balakrishnan@beehyv.com>
2026-03-12 15:24:52 +0530

    Refactor

--- backend/dristi-services/task-management/src/main/java/digit/service/TaskCreationService.java
+++ backend/dristi-services/task-management/src/main/java/digit/service/TaskCreationService.java
@@ -122,7 +122,7 @@ public class TaskCreationService {
             TaskDetails baseTaskDetails = buildSummonAndNoticeDetails(order, courtCase, partyType, taskManagement.getOrderItemId());
 
             log.info("Building task details list for {} party", partyType);
-            List<TaskDetails> taskDetailsList = buildTaskDetailsList(partyDetails, caseDetails, baseTaskDetails, complainantDetails);
+            List<TaskDetails> taskDetailsList = buildTaskDetailsList(partyDetails, caseDetails, baseTaskDetails, complainantDetails, courtCase);
             
             log.info("Building base task template");
             Task taskTemplate = buildBaseTask(taskManagement, order, courtCase, additionalDetails);
@@ -438,7 +438,7 @@ public class TaskCreationService {
 
     }
 
-    private List<TaskDetails> buildTaskDetailsList(PartyDetails party, CaseDetails caseDetails, TaskDetails baseTaskDetails, ComplainantDetails complainantDetails) {
+    private List<TaskDetails> buildTaskDetailsList(PartyDetails party, CaseDetails caseDetails, TaskDetails baseTaskDetails, ComplainantDetails complainantDetails, CourtCase courtCase) {
         List<TaskDetails> result = new ArrayList<>();
 
         if (party == null || party.getAddresses() == null || party.getAddresses().isEmpty()) {
@@ -481,7 +481,8 @@ public class TaskCreationService {
                                 .feePaidDate(channel != null ? channel.getFeePaidDate() : null)
                                 .isPendingCollection(channel != null &&
                                         RPAD.equalsIgnoreCase(channel.getChannelCode()) &&
-                                        channel.getFeePaidDate() != null
+                                        channel.getFeePaidDate() != null &&
+                                        !Boolean.TRUE.equals(courtCase.getIsLPRCase())
                                 )
                                 .build())
                         .build());
