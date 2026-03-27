152386adf51f38a503d6ad4652aceaf683c71cde
manimaarans <manimaaran@beehyv.com>
2026-03-27 20:07:00 +0530

    added suggested fix

--- backend/digit-services/egov-user/src/main/java/org/egov/user/security/SecurityConfig.java
+++ backend/digit-services/egov-user/src/main/java/org/egov/user/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.egov.user.security;
 
+import org.egov.user.security.oauth2.custom.CustomAuthenticationManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,12 +15,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private org.egov.user.security.oauth2.custom.CustomAuthenticationManager customAuthenticationManager;
+    private CustomAuthenticationManager customAuthenticationManager;
 
+    /**
+     * Expose the CustomAuthenticationManager as THE AuthenticationManager bean
+     * for the entire application, including the OAuth2 authorization server.
+     */
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return customAuthenticationManager;
+    }
+
+    /**
+     * Override the internal authentication manager builder to prevent
+     * WebSecurityConfigurerAdapter from creating its own default
+     * DaoAuthenticationProvider (which would intercept authentication
+     * before CustomAuthenticationManager).
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.parentAuthenticationManager(customAuthenticationManager);
     }
 
     @Override
