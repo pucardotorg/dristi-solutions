8ed6dafd7b01ef07adf4200fdece4711416a7792
TusharKatiyar2002 <163826002+TusharKatiyar2002@users.noreply.github.com>
2026-04-13 16:00:57 +0530

    removed _ from lodash and imported correct function (#6413) (#6433)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/NewBulkRescheduleTab.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/NewBulkRescheduleTab.js
@@ -9,7 +9,7 @@ import CustomCopyTextDiv from "@egovernments/digit-ui-module-dristi/src/componen
 import NewBulkRescheduleTable from "./NewBulkRescheduleTable";
 import { Urls } from "@egovernments/digit-ui-module-hearings/src/hooks/services/Urls";
 import { hearingService } from "@egovernments/digit-ui-module-hearings/src/hooks/services";
-import _ from "lodash";
+import get from "lodash/get";
 import axiosInstance from "@egovernments/digit-ui-module-core/src/Utils/axiosInstance";
 import { DateUtils } from "@egovernments/digit-ui-module-dristi/src/Utils";
 
@@ -162,7 +162,7 @@ const NewBulkRescheduleTab = ({ stepper, setStepper, selectedDate = new Date().s
     [{ name: "BulkRescheduleReason" }],
     {
       select: (data) => {
-        return _.get(data, "Hearing.BulkRescheduleReason", []).map((opt) => ({ ...opt }));
+        return get(data, "Hearing.BulkRescheduleReason", []).map((opt) => ({ ...opt }));
       },
     }
   );
