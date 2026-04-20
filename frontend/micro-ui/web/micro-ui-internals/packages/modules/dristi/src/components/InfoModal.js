140d0d47265ae13545d5b9b437c07be2f8d3b76f
utpalBeehyv <utpal.kant@beehyv.com>
2026-04-20 14:56:10 +0530

    feat: refactor InfoModal to include CloseBtn and Heading components directly (#6483)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/InfoModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/InfoModal.js
@@ -1,6 +1,34 @@
 import React from "react";
 import Modal from "./Modal";
-import { CloseBtn, Heading } from "./ModalComponents";
+import { CloseSvg } from "@egovernments/digit-ui-react-components";
+
+export const CloseBtn = (props) => {
+  return (
+    <div
+      onClick={props?.onClick}
+      style={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        paddingRight: "20px",
+        cursor: "pointer",
+        ...(props?.backgroundColor && { backgroundColor: props.backgroundColor }),
+        ...props?.style,
+      }}
+      className={props?.className}
+    >
+      <CloseSvg />
+    </div>
+  );
+};
+
+export const Heading = (props) => {
+  return (
+    <h1 className={props?.className || "heading-m"} style={props?.style}>
+      {props.label}
+    </h1>
+  );
+};
 
 const InfoModal = ({
   t,
