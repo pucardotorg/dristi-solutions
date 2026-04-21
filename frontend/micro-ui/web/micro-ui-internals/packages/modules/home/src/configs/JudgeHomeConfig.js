0d13c73e76495ade053a3678c1356082fb56ff47
utpalBeehyv <utpal.kant@beehyv.com>
2026-04-21 16:02:21 +0530

    Feature 5614 case life cycle uat (#6488)
    * Feature/case life cycle case stage clean up 5614 (#6406)
    
    * Add secondaryStage field to various home configuration files and update UI customizations for handling secondaryStage data
    
    * Refactor secondaryStage configuration in home modules to replace mdmsConfig with static options for improved clarity and maintainability
    
    * change mastername of mdms
    
    * rename substage
    
    * fix: update sorting logic for CaseSecondaryStage to use substage
    
    * refactor: streamline criteria handling for substage and secondaryStage in UICustomizations
    
    * refactor: simplify stage criteria in search configurations across home, courtroom, and judge modules
    
    * refactor: adjust select statements and remove stage criteria in search configurations for home, courtroom, and judge modules
    
    * refactor: update search configurations to replace 'substage' with 'stage' and adjust select logic for CasePrimaryStage across multiple modules
    
    * refactor: enhance search form handling by cleaning up criteria and ensuring consistent stage selection logic in UICustomizations
    
    * refactor: enhance select logic for CasePrimaryStage in search configurations across multiple modules to ensure consistent filtering and sorting
    
    * refactor: enhance select logic for CaseSecondaryStage in search configurations across multiple modules to improve filtering and sorting consistency
    
    * refactor: update search configurations to consistently replace 'substage' with 'stage' across multiple modules for improved clarity and functionality
    
    * refactor: add 'CS_SECONDARY_STAGE' configuration to search modules for improved filtering and customization
    
    ---------
    
    Co-authored-by: utpal.kant <utpal.kant@beehyv.com>
    
    * refactor: simplify case name rendering logic in UICustomizations for improved clarity (#6438)
    
    * Fix/case lifecycle dropdown 5646 (#6468)
    
    * fix: update HomeConfig and UICustomizations to use CaseUiPrimaryStage and adjust field names for dropdowns
    
    * fix: update dropdown configurations to use CaseUiPrimaryStage and adjust optionsKey for consistency across multiple components
    
    * fix: update UICustomizations to use 'name' instead of 'stage' for selectedStage in dropdown configurations
    
    * fix: enhance CaseUiPrimaryStage selection to sort items by name in JudgeHomeConfig
    
    * feat: add CaseUiPrimaryStage schema and initial data for case lifecycle management (#6472)
    
    * Feat/primary stage segregation 5666 (#6486)
    
    * refactor: update criteria structure in search configurations to use 'stage' instead of 'status' and enhance CaseUiPrimaryStage selection logic across multiple components
    
    * refactor: enhance search configurations by adding detailed criteria structure and refining CaseUiPrimaryStage selection logic across home, courtroom, and judge modules
    
    * refactor: unify search criteria structure across home, courtroom, and judge configurations by adding detailed 'stage' options for case processing
    
    ---------
    
    Co-authored-by: Piesieomane-beehyv <piesie.omane@beehyv.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/configs/JudgeHomeConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/configs/JudgeHomeConfig.js
@@ -2,6 +2,7 @@ const defaultSearchValues = {
   caseSearchText: "",
   caseType: "NIA S138",
   substage: "",
+  secondaryStage: "",
 };
 
 export const CaseWorkflowState = {
@@ -131,10 +132,7 @@ export const TabJudgeSearchConfig = {
         requestParam: {},
         requestBody: {
           tenantId: "pg",
-          criteria: {
-            stage: ["Pre-Trial", "Trial", "Post-Trial", "Long Pending Register"],
-            status: ["PENDING_REGISTRATION", "PENDING_ADMISSION", "CASE_ADMITTED", "PENDING_ADMISSION_HEARING", "PENDING_NOTICE", "PENDING_RESPONSE"],
-          },
+          criteria: {},
         },
         masterName: "commonUiConfig",
         moduleName: "homeJudgeUIConfig",
@@ -177,12 +175,37 @@ export const TabJudgeSearchConfig = {
                 type: "dropdown",
                 disable: false,
                 populators: {
-                  name: "substage",
-                  optionsKey: "code",
+                  name: "stage",
+                  optionsKey: "name",
                   mdmsConfig: {
-                    masterName: "SubStage",
+                    masterName: "CaseUiPrimaryStage",
                     moduleName: "case",
-                    select: "(data) => {return data['case'].SubStage?.map((item) => {return item});}",
+                    select:
+                      "(data) => {return data['case'].CaseUiPrimaryStage?.sort((a,b)=>a.name.localeCompare(b.name)).map((item) => {return item;});}",
+                  },
+                  styles: {
+                    maxWidth: "250px",
+                    minWidth: "200px",
+                  },
+                  optionsCustomStyle: {
+                    overflowX: "hidden",
+                  },
+                },
+              },
+              {
+                label: "CS_SECONDARY_STAGE",
+                isMandatory: false,
+                key: "secondaryStage",
+                type: "dropdown",
+                disable: false,
+                populators: {
+                  name: "secondaryStage",
+                  optionsKey: "substage",
+                  mdmsConfig: {
+                    masterName: "CaseSecondaryStage",
+                    moduleName: "case",
+                    select:
+                      "(data) => {return data['case'].CaseSecondaryStage?.map((item) => {return item}).filter((item) => item?.substage).filter((item, index, arr) => index === arr.findIndex((x) => (x?.substage || '').trim().toLowerCase() === (item?.substage || '').trim().toLowerCase())).sort((a,b) => (a?.substage || '').trim().localeCompare((b?.substage || '').trim()));}",
                   },
                   styles: {
                     maxWidth: "250px",
@@ -222,7 +245,12 @@ export const TabJudgeSearchConfig = {
               },
               {
                 label: "CS_STAGE",
-                jsonPath: "substage",
+                jsonPath: "stage",
+                additionalCustomization: true,
+              },
+              {
+                label: "CS_SECONDARY_STAGE",
+                jsonPath: "secondaryStage",
                 additionalCustomization: true,
               },
               {
@@ -258,8 +286,23 @@ export const TabJudgeSearchConfig = {
         requestBody: {
           tenantId: "pg",
           criteria: {
-            stage: ["Trial"],
-            status: ["PENDING_REGISTRATION", "PENDING_ADMISSION", "CASE_ADMITTED", "PENDING_ADMISSION_HEARING", "PENDING_NOTICE", "PENDING_RESPONSE"],
+            isLPRCase: false,
+            stage: [
+              "Long Pending Register",
+              "Post-Judgement",
+              "Judgement",
+              "Arguments",
+              "Defense Evidence",
+              "Examination of Accused",
+              "Complainant Evidence",
+              "Bail & Recording of Plea",
+              "Appearance",
+              "Cognizance",
+              "Registration",
+              "Defect Correction",
+              "Scrutiny",
+              "Filing",
+            ],
           },
         },
         masterName: "commonUiConfig",
@@ -303,12 +346,37 @@ export const TabJudgeSearchConfig = {
                 type: "dropdown",
                 disable: false,
                 populators: {
-                  name: "substage",
-                  optionsKey: "code",
+                  name: "stage",
+                  optionsKey: "name",
                   mdmsConfig: {
-                    masterName: "SubStage",
+                    masterName: "CaseUiPrimaryStage",
                     moduleName: "case",
-                    select: "(data) => {return data['case'].SubStage?.map((item) => {return item});}",
+                    select:
+                      "(data) => {return data['case'].CaseUiPrimaryStage?.sort((a,b)=>a.name.localeCompare(b.name)).map((item) => {return item;});}",
+                  },
+                  styles: {
+                    maxWidth: "250px",
+                    minWidth: "200px",
+                  },
+                  optionsCustomStyle: {
+                    overflowX: "hidden",
+                  },
+                },
+              },
+              {
+                label: "CS_SECONDARY_STAGE",
+                isMandatory: false,
+                key: "secondaryStage",
+                type: "dropdown",
+                disable: false,
+                populators: {
+                  name: "secondaryStage",
+                  optionsKey: "substage",
+                  mdmsConfig: {
+                    masterName: "CaseSecondaryStage",
+                    moduleName: "case",
+                    select:
+                      "(data) => {return data['case'].CaseSecondaryStage?.map((item) => {return item}).filter((item) => item?.substage).filter((item, index, arr) => index === arr.findIndex((x) => (x?.substage || '').trim().toLowerCase() === (item?.substage || '').trim().toLowerCase())).sort((a,b) => (a?.substage || '').trim().localeCompare((b?.substage || '').trim()));}",
                   },
                   styles: {
                     maxWidth: "250px",
@@ -348,7 +416,12 @@ export const TabJudgeSearchConfig = {
               },
               {
                 label: "CS_STAGE",
-                jsonPath: "substage",
+                jsonPath: "stage",
+                additionalCustomization: true,
+              },
+              {
+                label: "CS_SECONDARY_STAGE",
+                jsonPath: "secondaryStage",
                 additionalCustomization: true,
               },
               {
@@ -384,8 +457,19 @@ export const TabJudgeSearchConfig = {
         requestBody: {
           tenantId: "pg",
           criteria: {
-            stage: ["Pre-Trial"],
-            status: ["PENDING_REGISTRATION", "PENDING_ADMISSION", "PENDING_ADMISSION_HEARING", "PENDING_NOTICE", "PENDING_RESPONSE"],
+            stage: [
+              "Long Pending Register",
+              "Post-Disposal",
+              "Post-Judgement",
+              "Judgement",
+              "Arguments",
+              "Defense Evidence",
+              "Examination of Accused",
+              "Complainant Evidence",
+              "Bail & Recording of Plea",
+              "Appearance",
+              "Cognizance",
+            ],
           },
         },
         masterName: "commonUiConfig",
@@ -444,12 +528,37 @@ export const TabJudgeSearchConfig = {
                 type: "dropdown",
                 disable: false,
                 populators: {
-                  name: "substage",
-                  optionsKey: "code",
+                  name: "stage",
+                  optionsKey: "name",
                   mdmsConfig: {
-                    masterName: "SubStage",
+                    masterName: "CaseUiPrimaryStage",
                     moduleName: "case",
-                    select: "(data) => {return data['case'].SubStage?.map((item) => {return item});}",
+                    select:
+                      "(data) => {const excludedStages = ['Filing', 'Scrutiny', 'Defect Correction', 'Registration']; return data['case'].CaseUiPrimaryStage?.filter((item) => !excludedStages.includes((item?.name || '').trim())).sort((a,b)=>(a?.name || '').localeCompare(b?.name || '')).map((item) => {return item;});}",
+                  },
+                  styles: {
+                    maxWidth: "250px",
+                    minWidth: "200px",
+                  },
+                  optionsCustomStyle: {
+                    overflowX: "hidden",
+                  },
+                },
+              },
+              {
+                label: "CS_SECONDARY_STAGE",
+                isMandatory: false,
+                key: "secondaryStage",
+                type: "dropdown",
+                disable: false,
+                populators: {
+                  name: "secondaryStage",
+                  optionsKey: "substage",
+                  mdmsConfig: {
+                    masterName: "CaseSecondaryStage",
+                    moduleName: "case",
+                    select:
+                      "(data) => {return data['case'].CaseSecondaryStage?.map((item) => {return item}).filter((item) => item?.substage).filter((item, index, arr) => index === arr.findIndex((x) => (x?.substage || '').trim().toLowerCase() === (item?.substage || '').trim().toLowerCase())).sort((a,b) => (a?.substage || '').trim().localeCompare((b?.substage || '').trim()));}",
                   },
                   styles: {
                     maxWidth: "250px",
@@ -489,7 +598,12 @@ export const TabJudgeSearchConfig = {
               },
               {
                 label: "CS_STAGE",
-                jsonPath: "substage",
+                jsonPath: "stage",
+                additionalCustomization: true,
+              },
+              {
+                label: "CS_SECONDARY_STAGE",
+                jsonPath: "secondaryStage",
                 additionalCustomization: true,
               },
 
