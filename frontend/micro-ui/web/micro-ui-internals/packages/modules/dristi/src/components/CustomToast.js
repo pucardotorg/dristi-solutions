cd53e6efdee988220b75f8c076050e1aea2202ab
utpal.kant <utpal.kant@beehyv.com>
2026-05-04 15:59:10 +0530

    fix: adjust styles in CustomToast component for improved layout and appearance

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/CustomToast.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/CustomToast.js
@@ -107,6 +107,9 @@ const CustomToast = ({
               gap: "4px",
               transition: "background 0.2s",
               marginLeft: "10px",
+              marginTop: "0px",
+              whiteSpace: "nowrap",
+              width: "fit-content",
             }}
           >
             <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="2">
