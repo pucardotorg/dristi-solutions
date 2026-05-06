1135a871180a4a89e26e3f4b3dbd09bf66ffc629
utpalBeehyv <utpal.kant@beehyv.com>
2026-05-06 12:59:57 +0530

    fix: refactor date formatting to use DateUtils for consistency across components (#6591)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/configs/UICustomizations.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/configs/UICustomizations.js
@@ -810,11 +810,7 @@ export const UICustomizations = {
         case "PETITIONER":
           return <span>{value || ""}</span>;
         case "DATE_RAISED":
-          const date = value ? new Date(value) : new Date();
-          const day = date.getDate().toString().padStart(2, "0");
-          const month = (date.getMonth() + 1).toString().padStart(2, "0");
-          const year = date.getFullYear();
-          return <span>{`${day}-${month}-${year}`}</span>;
+          return <span>{DateUtils.getFormattedDate(value)}</span>;
         case "STATUS":
           return (
             <span
@@ -871,12 +867,7 @@ export const UICustomizations = {
         case "STATUS":
           return <CustomChip text={t(value)} shade={value === OrderWorkflowState.PENDING_BULK_E_SIGN && "orange"} />;
         case "DATE_ADDED":
-          const date = new Date(value);
-          const day = date.getDate().toString().padStart(2, "0");
-          const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month is zero-based
-          const year = date.getFullYear();
-          const formattedDate = `${day}-${month}-${year}`;
-          return <span>{value && value !== "0" ? formattedDate : ""}</span>;
+          return <span>{value && value !== "0" ? DateUtils.getFormattedDate(value) : ""}</span>;
         case "SELECT":
           return <BulkCheckBox rowData={row} colData={column} isBailBond={true} />;
         case "CS_ACTIONS":
@@ -974,12 +965,7 @@ export const UICustomizations = {
         case "PROCESS_TYPE":
           return t(value);
         case "DATE_CREATED":
-          const date = new Date(value);
-          const day = date.getDate().toString().padStart(2, "0");
-          const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month is zero-based
-          const year = date.getFullYear();
-          const formattedDate = `${day}-${month}-${year}`;
-          return <span>{value && value !== "0" ? formattedDate : ""}</span>;
+          return <span>{value && value !== "0" ? DateUtils.getFormattedDate(value) : ""}</span>;
         default:
           break;
       }
