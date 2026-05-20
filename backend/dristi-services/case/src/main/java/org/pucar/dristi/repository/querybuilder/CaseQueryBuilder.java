14aef14fb2556aaf1b00192c9466e9c9b979c416
anmolpawar121 <anmol.pawar@beehyv.com>
2026-05-20 17:37:17 +0530

    fix

--- backend/dristi-services/case/src/main/java/org/pucar/dristi/repository/querybuilder/CaseQueryBuilder.java
+++ backend/dristi-services/case/src/main/java/org/pucar/dristi/repository/querybuilder/CaseQueryBuilder.java
@@ -36,7 +36,9 @@ public class CaseQueryBuilder {
     private static final String ORDERBY_CLAUSE = " ORDER BY cases.{orderBy} {sortingOrder} ";
     private static final String DEFAULT_ORDERBY_CLAUSE = " ORDER BY cases.createdtime DESC ";
 
-    private static final String DOCUMENT_SELECT_QUERY_CASE = "SELECT doc.id AS id, " + "doc.documenttype AS documenttype, " + "doc.filestore AS filestore, " + "doc.documentuid AS documentuid, " + "doc.additionaldetails AS docadditionaldetails, " + "doc.case_id AS case_id, " + "doc.isactive AS isactive, " + "doc.linked_case_id AS linked_case_id, " + "doc.litigant_id AS litigant_id, " + "doc.representative_id AS representative_id, " + "doc.representing_id AS representing_id, " + "doc.poaholder_id AS poaholder_id ";
+    private static final String DOCUMENT_SELECT_QUERY_CASE = "Select doc.id as id, doc.documenttype as documenttype, doc.filestore as filestore," +
+            " doc.documentuid as documentuid, doc.additionaldetails as docadditionaldetails, doc.case_id as case_id, doc.isactive as isactive, doc.linked_case_id as linked_case_id, doc.litigant_id as litigant_id, doc.representative_id as representative_id, doc.representing_id as representing_id, doc.poaholder_id as poaholder_id ";
+
     private static final String FROM_DOCUMENTS_TABLE = " FROM dristi_case_document doc";
 
     private static final String TOTAL_COUNT_QUERY = "SELECT COUNT(*) FROM ({baseQuery}) total_result";
