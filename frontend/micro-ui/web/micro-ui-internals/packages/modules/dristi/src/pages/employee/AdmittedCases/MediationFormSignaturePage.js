a6919e4cef9a0235982b3d5267c870e411c1e9cc
ashishbeehyv <ashish.y@beehyv.com>
2026-05-27 15:30:53 +0530

    fix(MediationFormSignaturePage): refine file store ID validation logic for digitalization actions

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/AdmittedCases/MediationFormSignaturePage.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/employee/AdmittedCases/MediationFormSignaturePage.js
@@ -217,10 +217,18 @@ const MediationFormSignaturePage = () => {
         return party;
       });
       const initialFileStoreId = sessionStorage.getItem("InitialMediationFileStoreId");
-      const fileStoreCheck = digitalizationAction === MediationWorkflowAction.E_SIGN ? initialFileStoreId === mediationFileStoreId : true;
+      const fileStoreCheckOpen =
+        digitalizationAction === MediationWorkflowAction.E_SIGN
+          ? signatureDocumentId && signatureDocumentId !== mediationFileStoreId && initialFileStoreId === mediationFileStoreId
+          : true;
       if (isUserLoggedIn) {
         const fStoreId = signatureDocumentId || fileStoreId;
-        if (fStoreId && fStoreId !== mediationFileStoreId && fileStoreCheck) {
+        const fileStoreCheck =
+          digitalizationAction === MediationWorkflowAction.E_SIGN
+            ? fStoreId && fStoreId !== mediationFileStoreId && initialFileStoreId === mediationFileStoreId
+            : true;
+
+        if (fileStoreCheck) {
           await submissionService.updateDigitalization({
             digitalizedDocument: {
               ...digitalizationServiceDetails,
@@ -246,7 +254,7 @@ const MediationFormSignaturePage = () => {
           });
           setShowSuccessModal(true);
         }
-      } else if (signatureDocumentId && signatureDocumentId !== mediationFileStoreId && fileStoreCheck) {
+      } else if (fileStoreCheckOpen) {
         await submissionService.updateOpenDigitizedDocument({
           tenantId,
           documentNumber: documentNumber,
