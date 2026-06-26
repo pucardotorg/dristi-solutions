19c79e6b12930e1f1ddd109d3b5f3aa4f5a2fc87
Hemadri-Chinna <hemadri.pragada@beehyv.com>
2026-06-26 20:10:58 +0530

    Revert "dedupe task documents on warrant reissue regeneration"
    This reverts commit 4195dc0e6cbcaf73db766857a4366322bb487f5e.

--- backend/integration-services/summons-svc/src/main/java/digit/kafka/SummonsConsumer.java
+++ backend/integration-services/summons-svc/src/main/java/digit/kafka/SummonsConsumer.java
@@ -137,7 +137,7 @@ public class SummonsConsumer {
                 try {
                     log.info("Regenerating warrant PDF for reissue: taskNumber={}, action={}",
                             taskRequest.getTask().getTaskNumber(), action);
-                    summonsService.generateSummonsDocument(taskRequest, true);
+                    summonsService.generateSummonsDocument(taskRequest);
                 } catch (Exception e) {
                     log.error("Error regenerating warrant PDF for reissue: taskNumber={}",
                             taskRequest.getTask().getTaskNumber(), e);
