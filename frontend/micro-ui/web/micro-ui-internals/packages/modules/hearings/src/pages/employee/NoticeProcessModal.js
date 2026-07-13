0f695419a462f53bf9cc3ddcf48f9e9a05c017f0
ashishbeehyv <ashish.y@beehyv.com>
2026-07-13 16:41:28 +0530

    fix: include filingNumber in ReviewNoticeModal for accurate file naming

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/NoticeProcessModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/NoticeProcessModal.js
@@ -698,7 +698,7 @@ const NoticeProcessModal = ({
         <div>{modalContent}</div>
       )}
 
-      {showNoticeModal && <ReviewNoticeModal rowData={rowData} handleCloseNoticeModal={handleCloseNoticeModal} t={t} />}
+      {showNoticeModal && <ReviewNoticeModal filingNumber={filingNumber} rowData={rowData} handleCloseNoticeModal={handleCloseNoticeModal} t={t} />}
     </React.Fragment>
   );
 };
