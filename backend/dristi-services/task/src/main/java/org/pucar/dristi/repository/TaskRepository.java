2cf9cb14d8d82fa3c8bb189612c19628d24684a3
Mandar-Beehyv <sanjay.mandar@beehyv.com>
2026-07-16 17:32:00 +0530

    imported document

--- backend/dristi-services/task/src/main/java/org/pucar/dristi/repository/TaskRepository.java
+++ backend/dristi-services/task/src/main/java/org/pucar/dristi/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package org.pucar.dristi.repository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
+import org.pucar.dristi.web.models.Document;
 import org.egov.tracer.model.CustomException;
 import org.postgresql.util.PGobject;
 import org.pucar.dristi.repository.querybuilder.TaskCaseQueryBuilder;
